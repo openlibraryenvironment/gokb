9feb6360852613098091127e6e46d57147abd2d2
ianibo <ianibbo@googlemail.com>
2012-10-06 14:13:01 +0100

    updated

--- refine/gokb/module/scripts/extension.js
+++ refine/gokb/module/scripts/extension.js
@@ -3,7 +3,7 @@
 var GoKBExtension = { handlers: {} };
 
 
-GoKBExtension.handlers.browseToDataLoad = function() {
+GoKBExtension.handlers.editIngestProps = function() {
   // The form has to be created as part of the click handler. If you create it
   // inside the getJSON success handler, it won't work.
 
@@ -16,7 +16,23 @@ GoKBExtension.handlers.browseToDataLoad = function() {
   // document.body.appendChild(form);
   // var w = window.open("about:blank", "gokbproperties");
 
-  var dialog = $(DOM.loadHTML("ckan-storage-extension", "scripts/dialogs/gokb_ingest.html"));
+  // var dialog = $(DOM.loadHTML("gokb", "scripts/dialogs/gokb_ingest.html"));
+
+  // dialog.show()
+
+  // Some useful references at http://code.google.com/p/google-refine/source/browse/trunk/main/webapp/modules/core/scripts/dialogs/clustering-dialog.html
+  // and http://code.google.com/p/google-refine/source/browse/trunk/main/webapp/modules/core/scripts/dialogs/clustering-dialog.js
+
+  var self = this;
+
+  // https://code.google.com/p/google-refine/source/browse/trunk/main/webapp/modules/core/scripts/util/dom.js?spec=svn2356&r=2303
+  var dialog = $(DOM.loadHTML("freebase", "scripts/dialogs/freebase-loading-dialog.html"));
+
+  this.gokb_ingest_dlg_binding = DOM.bind(dialog);
+
+  this.gokb_ingest_dlg_binding.closeButton.click(function() { self._dismiss(); });
+
+  this._ingest_dlg = DialogSystem.showDialog(dialog);
 
   // $.getJSON(
   //   "/command/core/get-preference?" + $.param({ project: theProject.id, name: "freebase.load.jobID" }),
@@ -34,7 +50,7 @@ ExtensionBar.addExtensionMenu({
     {
       "id" : "GoKB/file-info",
       label: "Edit GoKB Ingest Properties",
-      click: function() { }
+      click: function() { GoKBExtension.handlers.editIngestProps(); }
     }
   ]
 });
