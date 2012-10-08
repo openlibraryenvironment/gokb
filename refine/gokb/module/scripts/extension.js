a5eef9b218fbc8ed79cf4e8786c9cc4b0c109b6e
ianibo <ianibbo@googlemail.com>
2012-10-08 10:45:26 +0100

    dialog showing again

--- refine/gokb/module/scripts/extension.js
+++ refine/gokb/module/scripts/extension.js
@@ -3,7 +3,7 @@
 var GoKBExtension = { handlers: {} };
 
 
-GoKBExtension.handlers.editIngestProps = function() {
+GoKBExtension.handlers.browseToDataLoad = function() {
   // The form has to be created as part of the click handler. If you create it
   // inside the getJSON success handler, it won't work.
 
@@ -16,27 +16,18 @@ GoKBExtension.handlers.editIngestProps = function() {
   // document.body.appendChild(form);
   // var w = window.open("about:blank", "gokbproperties");
 
-  // var dialog = $(DOM.loadHTML("gokb", "scripts/dialogs/gokb_ingest.html"));
+
+  var dialog = $(DOM.loadHTML("gokb", "scripts/dialogs/gokb_ingest.html"));
+  var dialog_bindings = DOM.bind(dialog);
+  // dialog_bindings.closeButton.click(function() {
+  //   dismiss();
+  // });
+  var res = DialogSystem.showDialog(dialog);
+
 
   // dialog.show()
+  alert("done");
 
-  // Some useful references at http://code.google.com/p/google-refine/source/browse/trunk/main/webapp/modules/core/scripts/dialogs/clustering-dialog.html
-  // and http://code.google.com/p/google-refine/source/browse/trunk/main/webapp/modules/core/scripts/dialogs/clustering-dialog.js
-
-  var self = this;
-
-  alert("OK");
-
-  // https://code.google.com/p/google-refine/source/browse/trunk/main/webapp/modules/core/scripts/util/dom.js?spec=svn2356&r=2303
-  var dialog = $(DOM.loadHTML("freebase", "scripts/dialogs/freebase-loading-dialog.html"));
-
-  alert("OK2");
-
-  this.gokb_ingest_dlg_binding = DOM.bind(dialog);
-
-  // this.gokb_ingest_dlg_binding.closeButton.click(function() { self._dismiss(); });
-
-  this._ingest_dlg = DialogSystem.showDialog(dialog);
 
   // $.getJSON(
   //   "/command/core/get-preference?" + $.param({ project: theProject.id, name: "freebase.load.jobID" }),
@@ -54,7 +45,9 @@ ExtensionBar.addExtensionMenu({
     {
       "id" : "GoKB/file-info",
       label: "Edit GoKB Ingest Properties",
-      click: function() { GoKBExtension.handlers.editIngestProps(); }
+      click: function() { 
+        GoKBExtension.handlers.browseToDataLoad();
+      }
     }
   ]
 });
