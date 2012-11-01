1a5c7654227f05b63a1ccb3aab2947ef4cdbda32
Steve Osguthorpe <steve@futurate.com>
2012-11-01 10:18:42 +0000

    # Improved dialog templating. # Merged bindings instead of rebinding whole object to ensure changes are preserved.

--- refine/gokb/module/scripts/extension.js
+++ refine/gokb/module/scripts/extension.js
@@ -7,18 +7,27 @@ GoKBExtension.createDialog = function(title, template) {
   var dialog_obj = $(DOM.loadHTML("gokb", "scripts/dialogs/gokb_dialog.html"));
   var dialog_bindings = DOM.bind(dialog_obj);
   
-  // Set title and content
-  dialog_bindings.dialogHeader.text(title);
-  dialog_bindings.dialogBody.prepend($(DOM.loadHTML("gokb", "scripts/dialogs/gokb_dialog_" + template + ".html")));
-  dialog_bindings = DOM.bind(dialog_obj);
+  // Set title if present
+  if (title) {
+	dialog_bindings.dialogHeader.text(title);
+  }
+  
+  // Set the content of the dialog if a template was supplied.
+  if (template) {
+	var body_template = $(DOM.loadHTML("gokb", "scripts/dialogs/gokb_dialog_" + template + ".html")); 
+	dialog_bindings.dialogLayout.prepend(body_template);
+	
+	// Add body template bindings.
+	$.extend(dialog_bindings, DOM.bind(body_template), dialog_bindings);
+  }
   
   // If there isn't a dialogContent div defined then add one here, don't forget the binding for future use.
   if ( !dialog_bindings.dialogContent ) {
 	var dc = $("<div bind='dialogContent' id='dialog-content' ></div>");
-	dialog_bindings.dialogBody.append(dc);
+	dialog_bindings.dialogLayout.append(dc);
 	
-	// Rebind.
-	dialog_bindings = DOM.bind(dialog_obj);
+	// Add dialog content bindings.
+	$.extend(dialog_bindings, DOM.bind(dc), dialog_bindings);
   }
   
   var dialog = {
@@ -48,6 +57,6 @@ GoKBExtension.handlers.suggest = function() {
   var dialog = GoKBExtension.createDialog("Suggested Transformations", "suggest");
   
   // Append some content.
-//  dialog.bindings.dialogContent.html("<ul><li>One suggestion</li></ul>");
+  dialog.bindings.dialogContent.html("<ul><li>One suggestion</li></ul>");
   GoKBExtension.showDialog(dialog);
 };
