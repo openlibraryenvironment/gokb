25eec67248f1461866d8ba5975f57f3168e680bd
ianibo <ianibbo@googlemail.com>
2012-11-02 12:22:14 +0000

    Installed spring security and generated auth domain components

--- server/gokb/grails-app/conf/Config.groovy
+++ server/gokb/grails-app/conf/Config.groovy
@@ -89,3 +89,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'org.gokb.cred.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'org.gokb.cred.UserRole'
+grails.plugins.springsecurity.authority.className = 'org.gokb.cred.Role'
