03bb333d99e17b0e0c11956549408932e64ee2af
ianibo <ianibbo@googlemail.com>
2012-11-05 12:25:54 +0000

    Create bootstrap admin user

--- server/gokb/grails-app/controllers/org/gokb/ApiController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/ApiController.groovy
@@ -1,6 +1,27 @@
 package org.gokb
 
+import grails.plugins.springsecurity.Secured
+import grails.converters.JSON
+
+
 class ApiController {
 
-    def index() { }
+  def index() { 
+  }
+  
+  @Secured(["ROLE_USER"])
+  def describe() {
+
+    def result = [
+      rules: [
+        [ name:'rule1', blurb:'blurb' ],
+        [ name:'rule2', blurb:'blurb' ],
+        [ name:'rule3', blurb:'blurb' ],
+        [ name:'rule4', blurb:'blurb' ],
+      ]
+    ]
+
+    render result as JSON
+  }
+
 }
