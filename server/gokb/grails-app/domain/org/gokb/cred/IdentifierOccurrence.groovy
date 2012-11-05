c19ef1678937cc3e422fe2b36d80cb817d16521d
ianibo <ianibbo@googlemail.com>
2012-11-05 11:04:33 +0000

    Link identifiers to components instead of specific subclasses

--- server/gokb/grails-app/domain/org/gokb/cred/IdentifierOccurrence.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/IdentifierOccurrence.groovy
@@ -5,22 +5,15 @@ class IdentifierOccurrence {
   Identifier identifier
 
   static belongsTo = [
-    ti:TitleInstance,
-    org:Org,
-    tipp:TitleInstancePackagePlatform
+    component:KBComponent
   ]
 
   static mapping = {
             id column:'io_id'
     identifier column:'io_canonical_id'
-            ti column:'io_ti_fk'
-          tipp column:'io_tipp_fk'
-           org column:'io_org_fk'
+     component column:'io_component_fk'
   }
 
   static constraints = {
-     org(nullable:true)
-      ti(nullable:true)
-    tipp(nullable:true)
   }
 }
