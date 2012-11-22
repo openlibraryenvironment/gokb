fa15da3f3ced6504d96fd4c28cd7c4352cdd77ed
Steve Osguthorpe <steve@futurate.com>
2012-11-22 15:53:27 +0000

    Added Refine to GOKb bridge framework for cross communication.

---
+++ refine/extensions/gokb/src/com/k_int/gokb/refine/A_RefineAPIBridge.java
@@ -0,0 +1,171 @@
+package com.k_int.gokb.refine;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+
+import com.google.refine.commands.Command;
+import com.google.refine.util.ParsingUtilities;
+
+
+public abstract class A_RefineAPIBridge extends Command {
+
+    private static final String POST_LINE_END         = "\r\n";
+
+    private static final String POST_HYPHENS          = "--";
+    private static final String POST_BOUNDARY         = "*****";
+    private static final String PROP_API_URL          = "http://localhost:8080/gokb/api/";
+    private static final int    PROP_TIMEOUT          = 1800000;
+    private static final int    POST_MAX_FILE_BUFFER  = 1*1024*1024;
+    private static void postFilesAndParams(HttpURLConnection conn, Properties params, File[] files) throws IOException {
+
+        DataOutputStream dos = new DataOutputStream( conn.getOutputStream() ); 
+        try {
+
+            // Add the params.
+            if (params != null) {
+                for (Object key : params.keySet()) {
+                    dos.writeBytes(POST_HYPHENS + POST_BOUNDARY + POST_LINE_END); 
+                    dos.writeBytes("Content-Disposition: form-data; name=\"" + key + "\"" + POST_LINE_END + POST_LINE_END);
+                    dos.writeBytes(ParsingUtilities.encode(params.getProperty((String)key, "")) + POST_LINE_END);
+                }
+            }    
+
+            // Add the files.
+            if (files != null) {
+                for (File file : files) {
+
+                    FileInputStream fileInputStream = new FileInputStream (file);
+
+                    try {
+                        // Send a binary file
+                        dos.writeBytes(POST_HYPHENS + POST_BOUNDARY + POST_LINE_END); 
+                        dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + file.getName() +"\"" + POST_LINE_END); 
+                        dos.writeBytes(POST_LINE_END); 
+
+                        // create a buffer of maximum size
+                        int bytesAvailable = fileInputStream.available(); 
+                        int bufferSize = Math.min(bytesAvailable, POST_MAX_FILE_BUFFER); 
+                        byte[] buffer = new byte[bufferSize]; 
+
+                        // read file and write it into form... 
+
+                        int bytesRead = fileInputStream.read(buffer, 0, bufferSize); 
+
+                        while (bytesRead > 0) 
+                        { 
+                            dos.write(buffer, 0, bufferSize); 
+                            bytesAvailable = fileInputStream.available(); 
+                            bufferSize = Math.min(bytesAvailable, POST_MAX_FILE_BUFFER); 
+                            bytesRead = fileInputStream.read(buffer, 0, bufferSize); 
+                        } 
+
+                        // send multipart form data necesssary after file data... 
+
+                        dos.writeBytes(POST_LINE_END);
+                    } finally {
+                        // close streams 
+                        fileInputStream.close();
+                    }
+                }
+            }
+
+            dos.writeBytes(POST_HYPHENS + POST_BOUNDARY + POST_HYPHENS + POST_LINE_END);
+            dos.flush();
+        } finally {
+            // Close the data stream.
+            dos.close();
+        }
+    }
+    
+    protected static String toParamString(Properties params) {
+        String pString = "";
+        if (params != null) {
+            for (Object key : params.keySet()) {
+                pString += 
+                        (!"".equals(pString) ? "&" : "?") + key + "=" + ParsingUtilities.encode(params.getProperty((String)key, ""));
+            }
+        }
+        return pString;
+    }
+    
+    protected final void doAPIGet (String apiMethodCall, Properties params) throws Exception{
+        doAPIGet (apiMethodCall, params, new RefineAPICallback());
+    }
+    
+    protected final void doAPIGet (String apiMethodCall, Properties params, RefineAPICallback callback) throws Exception {
+
+        // Return input stream.
+        InputStream inputStream = null;
+
+        // construct the url String
+        String urlString = PROP_API_URL + apiMethodCall + toParamString (params);
+
+        // Create a URL object.
+        URL url = new URL(urlString);
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection)url.openConnection();
+            connection.setConnectTimeout(PROP_TIMEOUT);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.connect();
+        } catch (Exception e) {
+            callback.onError (inputStream, new IOException("Cannot connect to " + urlString, e));
+        }
+
+        try {
+            inputStream = connection.getInputStream();
+            callback.onSuccess(inputStream);
+        } catch (Exception e) {
+            callback.onError(inputStream, new IOException("Cannot retrieve content from " + url, e));
+        } finally {
+            callback.complete(inputStream);
+        }
+    }
+
+    protected final void doAPIPost (String apiMethodCall, Properties params, File[] files, RefineAPICallback callback) throws Exception {
+        // Return input stream.
+        InputStream inputStream = null;
+
+        // construct the url String
+        String urlString = PROP_API_URL + apiMethodCall + toParamString (params);
+
+        // Create a URL object.
+        URL url = new URL(urlString);
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(PROP_TIMEOUT);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Connection", "Keep-Alive"); 
+            connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+POST_BOUNDARY);
+
+            // Do the POST
+            postFilesAndParams (connection, params, files);
+
+            connection.connect();
+        } catch (Exception e) {
+            callback.onError (inputStream, new IOException("Cannot connect to " + urlString, e));
+        }
+
+        try {
+            inputStream = connection.getInputStream();
+            callback.onSuccess(inputStream);
+        } catch (Exception e) {
+            callback.onError(inputStream, new IOException("Cannot retrieve content from " + url, e));
+        } finally {
+            callback.complete(inputStream);
+        }
+    }
+}
--- refine/extensions/gokb/src/com/k_int/gokb/refine/commands/CheckOutProject.java
+++ refine/extensions/gokb/src/com/k_int/gokb/refine/commands/CheckOutProject.java
@@ -2,8 +2,6 @@ package com.k_int.gokb.refine.commands;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -13,85 +11,58 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.k_int.gokb.refine.A_RefineAPIBridge;
+import com.k_int.gokb.refine.RefineAPICallback;
+
 import com.google.refine.ProjectManager;
 import com.google.refine.ProjectMetadata;
-import com.google.refine.commands.Command;
 import com.google.refine.model.Project;
 import com.google.refine.util.ParsingUtilities;
 
 
-public class CheckOutProject extends Command {
+public class CheckOutProject extends A_RefineAPIBridge {
     final static Logger logger = LoggerFactory.getLogger("GOKb-checkout-project_command");
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        ProjectManager pm = ProjectManager.singleton;
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {       
+
+        final ProjectManager pm = ProjectManager.singleton;
         pm.setBusy(true);
         try {
-            Properties options = ParsingUtilities.parseUrlParameters(request);
+            Properties params = ParsingUtilities.parseUrlParameters(request);
 
-            long projectID = Project.generateID();
+            final long projectID = Project.generateID();
             logger.info("Checking out GOKb project into Refine project {}", projectID);
 
-            downloadGOKbProject(request, options, projectID);
+            // Call the project download method with our callback to import the project.
+            doAPIGet("projectCheckout", params, new RefineAPICallback() {
 
-            pm.loadProjectMetadata(projectID);
+                @Override
+                protected void onSuccess(InputStream result) throws Exception {
+
+                    // Import the project
+                    pm.importProject(projectID, result, true);
+
+                    // Trya and load the meta-data
+                    pm.loadProjectMetadata(projectID);
+
+                    ProjectMetadata meta = pm.getProjectMetadata(projectID);
+
+                    if (meta != null) {
+                        // Move to project page.
+                        redirect(response, "/project?project=" + projectID);
+                    } else {
+                        logger.error("Failed to import project. Reason unknown.");
+                    }
+                }
+            });
+        } catch (Exception e) {
             
-            ProjectMetadata meta = pm.getProjectMetadata(projectID);
-            
-            if (meta != null) {
-                 // Move to project page.
-                redirect(response, "/project?project=" + projectID);
-            } else {
-                logger.error("Failed to import project. Reason unknown.");
-            }
-        } catch (Exception e) {
-            respondException(response, e);
+            // Respond with the error page.
+            respondWithErrorPage(request, response, e.getLocalizedMessage(), e);
         } finally {
-            ProjectManager.singleton.setBusy(false);
-        }
-    }
-    
-    /**
-     * Import the Project from the GOKb repository
-     */
-    protected void downloadGOKbProject (
-            HttpServletRequest    request,
-            Properties            options,
-            long projectID
-    ) throws Exception {
-        
-        String urlString = "";
-        for (Object key : options.keySet()) {
-            urlString += 
-              (!"".equals(urlString) ? "&" : "?") + key + "=" + ParsingUtilities.encode(options.getProperty((String)key, ""));
-        }
-        
-        urlString = "http://localhost:8080/gokb/api/projectCheckout" + urlString;
-        
-        URL url = new URL(urlString);
-        URLConnection connection = null;
-
-        try {
-            connection = url.openConnection();
-            connection.setConnectTimeout(180000);
-            connection.connect();
-        } catch (Exception e) {
-            throw new Exception("Cannot connect to " + urlString, e);
-        }
-
-        InputStream inputStream = null;
-        try {
-            inputStream = connection.getInputStream();
-        } catch (Exception e) {
-            throw new Exception("Cannot retrieve content from " + url, e);
-        }
-
-        try {
-            ProjectManager.singleton.importProject(projectID, inputStream, !urlString.endsWith(".tar"));
-        } finally {
-            inputStream.close();
+            pm.setBusy(false);
         }
     }
 }
