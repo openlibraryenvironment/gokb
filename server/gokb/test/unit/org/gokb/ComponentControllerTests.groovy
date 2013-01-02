df217e82c42d1a7be8cbac46f3aa5bd5e493e92f
Ian Ibbotson <ianibbo@gmail.com>
2013-01-02 13:26:52 +0000

    updated

---
+++ server/gokb/test/unit/org/gokb/ComponentControllerTests.groovy
@@ -0,0 +1,17 @@
+package org.gokb
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ComponentController)
+class ComponentControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
