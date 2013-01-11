147d97a807a5fffa6448f57b21e4c0e5a75a968e
ianibo <ianibbo@googlemail.com>
2013-01-11 09:28:51 +0000

    Corrected combo errors

--- server/gokb/grails-app/domain/org/gokb/cred/Combo.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/Combo.groovy
@@ -6,26 +6,24 @@ class Combo {
   RefdataValue type
 
   // Participant 1 - One of these
-  Org fromOrg
+  KBComponent from
 
   // Participant 2 - One of these
-  Org toOrg
-
-
+  KBComponent to
 
   static mapping = {
                 id column:'combo_id'
            version column:'combo_version'
             status column:'combo_status_rv_fk'
               type column:'combo_type_rv_fk'
-           fromOrg column:'combo_from_org_fk'
-             toOrg column:'combo_to_org_fk'
+           fromOrg column:'combo_from_fk'
+             toOrg column:'combo_to_fk'
   }
 
   static constraints = {
     status(nullable:true, blank:false)
     type(nullable:true, blank:false)
-    fromOrg(nullable:true, blank:false)
-    toOrg(nullable:true, blank:false)
+    from(nullable:true, blank:false)
+    to(nullable:true, blank:false)
   }
 }
