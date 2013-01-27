7fb9a0b82d0560e6c90d7c121af185f139a87626
Ian Ibbotson <ianibbo@gmail.com>
2013-01-27 12:32:05 +0000

    Wired in simple field edit

--- server/gokb/grails-app/controllers/org/gokb/AjaxSupportController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/AjaxSupportController.groovy
@@ -4,9 +4,30 @@ import grails.converters.JSON
 
 class AjaxSupportController {
 
+  def genericOIDService
+
   def edit() { 
+    // edit [name:name, value:project:12, pk:org.gokb.cred.Package:2950, action:edit, controller:ajaxSupport]
     log.debug("edit ${params}");
     def result = [:]
+
+    try {
+      if ( params.pk ) {
+        def target = genericOIDService.resolveOID(params.pk)
+        if ( target ) {
+          target[params.name] = params.value
+          target.save(flush:true)
+        }
+
+        pk_components = pk.split(':')
+        if ( pk_components.length == 2 ) {
+        }
+      }
+    }
+    catch ( Exception e ) {
+      log.error(e)
+    }
+
     render result as JSON
   }
 }
