9b52df93e99377e956da4c86dc4bceed3dcfd74d
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-04-05 13:17:39 +0100

    Syntax error "fuish"

--- server/gokb/grails-app/domain/org/gokb/cred/RefdataCategory.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/RefdataCategory.groovy
@@ -31,7 +31,7 @@ class RefdataCategory {
     def result = RefdataValue.findByOwnerAndValue(cat, value)
 
     if ( !result ) {
-      new RefdataValue(owner:cat, value:value).save(fliush:true);
+      new RefdataValue(owner:cat, value:value).save(flush:true);
       result = RefdataValue.findByOwnerAndValue(cat, value);
     }
 
