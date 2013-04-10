624a08428e5d063779ebbf10efde1eedfb9227df
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-04-10 15:18:55 +0100

    On document ready call needed to enable editiable components/taglib now moved to its own js resource. To use edtiable tags, make sure you r:require the editable resource. Should pull in all the necessary components.

--- server/gokb/grails-app/conf/ApplicationResources.groovy
+++ server/gokb/grails-app/conf/ApplicationResources.groovy
@@ -5,6 +5,7 @@ modules = {
   }
   editable {
     resource url:'css/select2.css'
+    resource url:'js/editable.js.gsp'
     resource url:'js/moment.min.js'
     resource url:'js/select2.min.js'
   }
