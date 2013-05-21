36a3c8bb18df5bd9bf27f39590ec3fc2dcd9e267
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-05-21 13:43:32 +0100

    Using groovy style method declaration.

--- server/gokb/src/groovy/org/gokb/cred/actions/PublisherTransfer.groovy
+++ server/gokb/src/groovy/org/gokb/cred/actions/PublisherTransfer.groovy
@@ -3,15 +3,8 @@ package org.gokb.cred.actions
 class PublisherTransfer extends A_Action {
 
   @Override
-  public Object doAction(Object param) {
+  public Object doAction(def param = []) {
     // TODO Auto-generated method stub
     return null;
   }
-
-  @Override
-  public Object doAction() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
 }
