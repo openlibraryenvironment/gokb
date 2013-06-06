5b946a93a61541034dbdb2eac59d3cac20702027
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-06-06 16:10:42 +0100

    Incremented to version 0.7 of refine module.

--- refine/extensions/gokb/src/com/k_int/gokb/refine/GOKbModuleImpl.java
+++ refine/extensions/gokb/src/com/k_int/gokb/refine/GOKbModuleImpl.java
@@ -29,7 +29,7 @@ public class GOKbModuleImpl extends ButterflyModuleImpl {
     public static GOKbModuleImpl singleton;
     public static ExtendedProperties properties;
 
-    public static final String VERSION = "0.6";
+    public static final String VERSION = "0.7";
 
     @Override
     public void init(ServletConfig config) throws Exception {
