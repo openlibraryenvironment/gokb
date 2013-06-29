cdfa28647f5a3c225c369cb5b54098b8260df6ef
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-06-29 09:16:19 +0100

    Added variant name, moved name normalisation out to service

---
+++ server/gokb/grails-app/domain/org/gokb/cred/KBComponentVariantName.groovy
@@ -0,0 +1,44 @@
+package org.gokb.cred
+
+class KBComponentVariantName {
+
+  KBComponent owner
+  RefdataValue variantType
+  RefdataValue locale
+
+  String variantName
+  String normVariantName
+
+  static mapping = {
+        id column:'cvn_id'
+        version column:'cvn_version'
+        owner column:'cvn_kbc_fk'
+        variantName column:'cvn_variant_name'
+        normVariantName column:'cvn_norm_variant_name'
+        variantType column:'cvn_type_rv_fk'
+        locale column:'cvn_locale_rv_fk'
+  }
+
+  static constraints = {
+        variantName (nullable:false, blank:false, maxSize:2048)
+        normVariantName  (nullable:false, blank:false, maxSize:2048)
+        variantType (nullable:true, blank:false)
+        locale (nullable:true, blank:false)
+        owner (nullable:false, blank:false)
+  }
+
+  protected def generateNormname () {
+    normVariantName = textNormalisationService.normalise(variantName);
+  }
+
+  def beforeInsert() {
+    // Generate the any necessary values.
+    generateNormname()
+  }
+
+  def beforeUpdate() {
+    generateNormname()
+  }
+
+
+}
---
+++ server/gokb/grails-app/services/org/gokb/TextNormalisationService.groovy
@@ -0,0 +1,8 @@
+package org.gokb
+
+class TextNormalisationService {
+
+  def normalise(value) {
+    return value.toLowerCase().trim()
+  }
+}
