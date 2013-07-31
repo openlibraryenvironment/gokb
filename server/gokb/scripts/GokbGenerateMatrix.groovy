a652b512e14927844839f0ce9febf6502c7c9b11
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-07-31 17:31:41 +0100

    added gdocs

--- server/gokb/scripts/GokbGenerateMatrix.groovy
+++ server/gokb/scripts/GokbGenerateMatrix.groovy
@@ -26,7 +26,7 @@ target(main: "Generate the property matrix for KBComponents") {
     
 
     component_class.getSubClasses().each { csc ->
-      grailsConsole.addStatus "Handle component subclass: ${csc.fullName}"
+      // grailsConsole.addStatus "Handle component subclass: ${csc.fullName}"
       csc.getProperties().each { cscp ->
         if ( propertyNames.contains( cscp.name ) ) {
           // no action - Property name already in top row of matrix
@@ -37,7 +37,7 @@ target(main: "Generate the property matrix for KBComponents") {
       }
     }
 
-    grailsConsole.addStatus "Full list of properties is ${propertyNames}"
+    // grailsConsole.addStatus "Full list of properties is ${propertyNames}"
 
     def matrix = []  // List, 1 row per class
 
@@ -46,7 +46,51 @@ target(main: "Generate the property matrix for KBComponents") {
       matrix.add(genMatrixRow(csc, propertyNames))
     }
 
-    println(matrix)
+
+    //println "wrapping file ${file.name}"
+
+    //        echo(file: "src/docs/ref/Answers/${file.name}.gdoc", """
+    //            {code}
+    //                ${file.getText()}
+    //            {code}
+    //        """)
+
+    def x=1
+    def y=1
+    def m2 = new String[propertyNames.size()+1][matrix.size()+1]
+
+    propertyNames.each { p ->
+      m2[x++][0] = p
+    }
+  
+    matrix.each { m ->
+      x=0
+      m.each { p ->
+        m2[x++][y] = p
+      }
+      y++
+    }
+
+    File f = new File("src/docs/guide/gokbMatrix.gdoc")
+    f.delete()
+    f.append('This is the gokb KBComponent matrix')
+    f.append('{table}\n');
+    m2.each { row ->
+      def first=true
+      row.each { v ->
+        if ( first ) {
+          first=false
+        }
+        else {
+          f.append(" | ");
+        }
+        f.append("* ${v?:''} *");
+      }
+      f.append("\n");
+    }
+
+    // 
+    f.append('{table}');
   }
   catch ( Exception e ) {
     grailsConsole.addStatus e.message
