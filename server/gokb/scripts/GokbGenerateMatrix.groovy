10a84d3e10f0ef446e1fe1a2e5e56780aa383848
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-07-31 16:53:40 +0100

    gokb-generate-matrix collects all needed data

--- server/gokb/scripts/GokbGenerateMatrix.groovy
+++ server/gokb/scripts/GokbGenerateMatrix.groovy
@@ -10,23 +10,62 @@ target(main: "Generate the property matrix for KBComponents") {
   depends( configureProxy, packageApp, classpath, loadApp, configureApp )
 
   try {
-    def component_class = ApplicationHolder.application.getClassForName("org.gokb.cred.KBComponent")
+    //def component_class = ApplicationHolder.application.getClassForName("org.gokb.cred.KBComponent")
+    def component_class = ApplicationHolder.application.getDomainClass('org.gokb.cred.KBComponent')
     grailsConsole.addStatus "Got KBComponent class(${component_class})... list subclasses"
 
-    for (dc in ApplicationHolder.application.domainClasses) { 
-       grailsConsole.addStatus dc.fullName // full name with package 
-       grailsConsole.addStatus dc.name // simple class name 
-    } 
+    // for (dc in ApplicationHolder.application.domainClasses) { 
+    //    grailsConsole.addStatus dc.fullName // full name with package 
+    //    grailsConsole.addStatus dc.name // simple class name 
+    //    if ( dc.isAssignableFrom(org.gokb.cred.KBComponent.class) ) {
+    //      grailsConsole.addStatus '** Subclass of component'
+    //    }
+    // } 
 
-
+    def propertyNames = []
     
-    // do something with connection
+
+    component_class.getSubClasses().each { csc ->
+      grailsConsole.addStatus "Handle component subclass: ${csc.fullName}"
+      csc.getProperties().each { cscp ->
+        if ( propertyNames.contains( cscp.name ) ) {
+          // no action - Property name already in top row of matrix
+        }
+        else {
+          propertyNames.add(cscp.name)
+        }
+      }
+    }
+
+    grailsConsole.addStatus "Full list of properties is ${propertyNames}"
+
+    def matrix = []  // List, 1 row per class
+
+    matrix.add(genMatrixRow(component_class, propertyNames))
+    component_class.getSubClasses().each { csc ->
+      matrix.add(genMatrixRow(csc, propertyNames))
+    }
+
+    println(matrix)
+  }
+  catch ( Exception e ) {
+    grailsConsole.addStatus e.message
   }
   finally {
     grailsConsole.addStatus 'GokbGenerateMatrix completed'
   }
  
   depends(docs)
+}
+
+def genMatrixRow(cls,propnames) {
+  def newrow = new String[propnames.size()+1]
+  newrow[0]=cls.fullName
+  cls.getProperties().each { clsp ->
+    def idx = propnames.indexOf(clsp.name)
+    newrow[idx+1]='Y'
+  }
+  newrow
 }
 
 printMessage = { String message -> event('StatusUpdate', [message]) }
