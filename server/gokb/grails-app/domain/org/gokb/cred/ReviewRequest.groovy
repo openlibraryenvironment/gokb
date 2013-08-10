446ba1d96285679b130dcb02ef0b88ea8b072fb2
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-08-10 16:29:26 +0100

    Removed requestTimestamp as the component is auditable and has a timestamp automatically.
    Added raisedBy and reviewedBy users and also created static helper method to raise a request for a component.

--- server/gokb/grails-app/domain/org/gokb/cred/ReviewRequest.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/ReviewRequest.groovy
@@ -1,12 +1,15 @@
 package org.gokb.cred
 
 class ReviewRequest {
+  
+  static auditable = true
 
   KBComponent componentToReview
   String descriptionOfCause
   String reviewRequest
-  Date requestTimestamp
   RefdataValue status
+  User reviewedBy
+  User raisedBy
 
   // Timestamps
   Date dateCreated
@@ -23,10 +26,28 @@ class ReviewRequest {
     componentToReview(nullable:false, blank:false)
     descriptionOfCause(nullable:true, blank:true)
     reviewRequest(nullable:false, blank:false)
-    requestTimestamp(nullable:false, blank:false)
     status(nullable:false, blank:false)
+    raisedBy(nullable:true, blank:false)
+    reviewedBy(nullable:true, blank:false)
     dateCreated(nullable:true, blank:true)
     lastUpdated(nullable:true, blank:true)
+  }
+  
+  public static ReviewRequest raise (KBComponent forComponent, String actionRequired, String cause = null, User raisedBy = null) {
+	
+	// Create a request.
+	ReviewRequest req = new ReviewRequest (
+		status	: RefdataCategory.lookupOrCreate('ReviewRequest.status', 'Needs Review'),
+		raisedBy : (raisedBy),
+		descriptionOfCause : (cause),
+		reviewRequest : (actionRequired)
+	)
+	
+	// Add to the list of requests for the component.
+	forComponent.adToReviewRequests( req )
+	
+	// Just return the request.
+	req
   }
 
 }
