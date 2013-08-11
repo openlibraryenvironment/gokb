b3f823868c9e64eb56fbcec33eaf0188ef789526
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-08-11 16:35:09 +0100

    Double escape the slash.

--- refine/extensions/gokb/module/scripts/project/validation-panel.js
+++ refine/extensions/gokb/module/scripts/project/validation-panel.js
@@ -49,13 +49,13 @@ ValidationPanel.prototype.update = function(onDoneFunc) {
       		
       		if ("result" in data && data.result.length > 0) {
       			// (cells['publicationtitle'].value + cells['package.name'].value).match('\\QAfrican and Asian StudiesBrill:Master:2013\\E|\\QAfrican DiasporaBrill:Master:2013\\E') != null
-      			var grel = "(cells['publicationtitle'].value + cells['package.name'].value).match('\\Q";
+      			var grel = "(cells['publicationtitle'].value + cells['package.name'].value).match('\\\\Q";
       			for (var i=0; i<data.result.length;i++) {
-      				grel += (i > 0 ? '\\E|\\Q' : "") + data.result[i];
+      				grel += (i > 0 ? '\\\\E|\\\\Q' : "") + data.result[i];
       			}
       			
       			// Close the statement.
-      			grel += "\\E') != null";
+      			grel += "\\\\E') != null";
       			
       			// Add a validation message here.
       			self.data.dataCheck.messages.push(
