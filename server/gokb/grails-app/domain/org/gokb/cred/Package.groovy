d25fcb9b9e723b0b6bc5fef3e0ff46d2404dff50
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-08-14 14:56:03 +0100

    Outed tabs

--- server/gokb/grails-app/domain/org/gokb/cred/Package.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/Package.groovy
@@ -25,58 +25,58 @@ class Package extends KBComponent {
   RefineProject lastProject
   
   private static refdataDefaults = [
-	"scope" 	: "Front File",
-	"listStatus"	: "Checked",
-	"breakable"	: "Unknown",
-	"consistent"	: "Unknown",
-	"fixed"		: "Unknown",
-	"paymentType"	: "Unknown",
-	"global"	: "Global"
+    "scope"   : "Front File",
+    "listStatus"  : "Checked",
+    "breakable"  : "Unknown",
+    "consistent"  : "Unknown",
+    "fixed"    : "Unknown",
+    "paymentType"  : "Unknown",
+    "global"  : "Global"
   ]
   
   static manyByCombo = [
-	tipps 				: TitleInstancePackagePlatform,
-	children			: Package,
-	territories			: Territory
+    tipps         : TitleInstancePackagePlatform,
+    children      : Package,
+    territories      : Territory
   ]
   
   static hasByCombo = [
-	parent				: Package,
-	broker				: Org,
-	provider			: Org,
-	licensor			: Org,
-	vendor				: Org,
-	nominalPlatform		: Platform,
-	'previous'     		: Package,
-	successor     		: Package
+             parent : Package,
+             broker : Org,
+           provider : Org,
+           licensor : Org,
+             vendor : Org,
+    nominalPlatform : Platform,
+         'previous' : Package,
+          successor : Package
   ]
   
   static mappedByCombo = [
-	children    : 'parent',
-	successor	: 'previous',
+     children : 'parent',
+    successor : 'previous',
   ]
 
   static mapping = {
     listStatus column:'pkg_list_status_rv_fk'
-	lastProject column:'pkg_refine_project_fk'
-	scope column:'pkg_scope_rv_fk'
-	breakable column:'pkg_breakable_rv_fk'
-	consistent column:'pkg_consistent_rv_fk'
-	fixed column:'pkg_fixed_rv_fk'
-	paymentType column:'pkg_payment_type_rv_fk'
-	global column:'pkg_global_rv_fk'
+    lastProject column:'pkg_refine_project_fk'
+    scope column:'pkg_scope_rv_fk'
+    breakable column:'pkg_breakable_rv_fk'
+    consistent column:'pkg_consistent_rv_fk'
+    fixed column:'pkg_fixed_rv_fk'
+    paymentType column:'pkg_payment_type_rv_fk'
+    global column:'pkg_global_rv_fk'
   }
 
   static constraints = {
-	lastProject		(nullable:true, blank:false)
-	scope 			(nullable:true, blank:false)
-	listStatus		(nullable:true, blank:false)
-	breakable		(nullable:true, blank:false)
-	consistent		(nullable:true, blank:false)
-	fixed			(nullable:true, blank:false)
-	paymentType		(nullable:true, blank:false)
-	global			(nullable:true, blank:false)
-	lastProject		(nullable:true, blank:false)
+    lastProject    (nullable:true, blank:false)
+    scope       (nullable:true, blank:false)
+    listStatus    (nullable:true, blank:false)
+    breakable    (nullable:true, blank:false)
+    consistent    (nullable:true, blank:false)
+    fixed      (nullable:true, blank:false)
+    paymentType    (nullable:true, blank:false)
+    global      (nullable:true, blank:false)
+    lastProject    (nullable:true, blank:false)
   }
 
 //  @Transient
