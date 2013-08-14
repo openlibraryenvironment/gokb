69cf750fcf3b8a65b8703f2b1880cf1d5f732b34
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-08-14 16:28:28 +0100

    Added nulls to constructor.

--- server/gokb/src/groovy/org/gokb/validation/types/IsOneOf.groovy
+++ server/gokb/src/groovy/org/gokb/validation/types/IsOneOf.groovy
@@ -4,12 +4,17 @@ class IsOneOf extends CellMatches {
   
   private static final String ERROR_TYPE = "data_invalid"
   
-  public IsOneOf (String columnName, String severity, Set<String> items, boolean case_sensitive = false) {
-	super (columnName, severity)
+  public IsOneOf (String columnName, String severity, Collection<String> items, boolean case_sensitive = false) {
 	
-	this.regex = (case_sensitive ? "" : "(?i)") + "^(\\Q${items.join('\\E|\\Q')}\\E)\$"
+	// Pass nulls to the constructor as we are going to set them afterwards.
+	super (columnName, severity, null, null, null)
+	
+	// Make a set 
+	Set<String> all_items = items as Set 
+	
+	this.regex = (case_sensitive ? "" : "(?i)") + "^(\\Q${all_items.join('\\E|\\Q')}\\E)\$"
 	this.text = "One or more rows contain invalid values in the column \"${columnName}\"." +
-		"Allowed values are \"${items.join('\", \"')}\". Values are ${(case_sensitive ? '' : 'not ')}case sensitive."
+		"Allowed values are \"${all_items.join('\", \"')}\". Values are ${(case_sensitive ? '' : 'not ')}case sensitive."
 	this.facetValue = "and (isNonBlank(value), value.match(/${regex}/))"
   }
 }
