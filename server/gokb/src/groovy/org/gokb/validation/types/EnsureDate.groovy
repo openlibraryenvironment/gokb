ff6e14e42402122eeac81e701488c59a30202ebb
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-08-15 08:18:03 +0100

    Return true in facet by default.

--- server/gokb/src/groovy/org/gokb/validation/types/EnsureDate.groovy
+++ server/gokb/src/groovy/org/gokb/validation/types/EnsureDate.groovy
@@ -28,7 +28,7 @@ class EnsureDate extends A_ValidationRule implements I_RowValidationRule {
 	return [
 	  col			: columnName,
 	  text			: "One or more rows contains invalid dates in the column \"${columnName}\".",
-	  facetValue	: "if (isNonBlank(value), (value.toDate().toString() != value.toString()), true)",
+	  facetValue	: "if (isNonBlank(value), (value.toDate().toString() != value.toString()), false)",
 	  facetName		: "Invalid dates in ${columnName}"
 	]
   }
