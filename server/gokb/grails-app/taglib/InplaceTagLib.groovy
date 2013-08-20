cca03b2bc07a114d7b95b1113a8b82dc2d6b2c05
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-08-20 12:06:25 +0100

    removed apparent controller method from taglib

--- server/gokb/grails-app/taglib/InplaceTagLib.groovy
+++ server/gokb/grails-app/taglib/InplaceTagLib.groovy
@@ -55,11 +55,12 @@ class InplaceTagLib {
     def update_link = createLink(controller:'ajaxSupport', action: 'genericSetRel')
     def oid = attrs.owner.id != null ? "${attrs.owner.class.name}:${attrs.owner.id}" : ''
     def id = attrs.id ?: "${oid}:${attrs.field}"
+    def type = attrs.type ?: "select"
    
     out << "<span>"
    
     // Output an editable link
-    out << "<span id=\"${id}\" class=\"ipe\" data-pk=\"${oid}\" data-type=\"select\" data-name=\"${attrs.field}\" data-source=\"${data_link}\" data-url=\"${update_link}\">"
+    out << "<span id=\"${id}\" class=\"ipe\" data-pk=\"${oid}\" data-type=\"${type}\" data-name=\"${attrs.field}\" data-source=\"${data_link}\" data-url=\"${update_link}\">"
 
     // Here we can register different ways of presenting object references. The most pressing need to be
     // outputting a span containing an icon for refdata fields.
@@ -164,68 +165,4 @@ class InplaceTagLib {
     out << body()
     out << "</a>";
   }
-
-  def addToCollection() {
-    log.debug("AjaxController::addToCollection ${params}");
-
-    def contextObj = resolveOID2(params.__context)
-    def domain_class = grailsApplication.getArtefact('Domain',params.__newObjectClass)
-
-    if ( domain_class ) {
-
-      if ( contextObj ) {
-        log.debug("Create a new instance of ${params.__newObjectClass}");
-
-        def new_obj = domain_class.getClazz().newInstance();
-
-        domain_class.getPersistentProperties().each { p -> // list of GrailsDomainClassProperty
-          log.debug("${p.name} (assoc=${p.isAssociation()}) (oneToMany=${p.isOneToMany()}) (ManyToOne=${p.isManyToOne()}) (OneToOne=${p.isOneToOne()})");
-          if ( params[p.name] ) {
-            if ( p.isAssociation() ) {
-              if ( p.isManyToOne() || p.isOneToOne() ) {
-                // Set ref property
-                log.debug("set assoc ${p.name} to lookup of OID ${params[p.name]}");
-                // if ( key == __new__ then we need to create a new instance )
-                new_obj[p.name] = resolveOID2(params[p.name])
-              }
-              else {
-                // Add to collection
-                log.debug("add to collection ${p.name} for OID ${params[p.name]}");
-                new_obj[p.name].add(resolveOID2(params[p.name]))
-              }
-            }
-            else {
-              log.debug("Set simple prop ${p.name} = ${params[p.name]}");
-              new_obj[p.name] = params[p.name]
-            }
-          }
-        }
-
-        if ( params.__recip ) {
-          log.debug("Set reciprocal property ${params.__recip} to ${contextObj}");
-          new_obj[params.__recip] = contextObj
-        }
-
-        log.debug("Saving ${new_obj}");
-
-        if ( new_obj.save() ) {
-          log.debug("Saved OK");
-        }
-        else {
-          new_obj.errors.each { e ->
-            log.debug("Problem ${e}");
-          }
-        }
-      }
-      else {
-        log.debug("Unable to locate instance of context class with oid ${params.__context}");
-      }
-    }
-    else {
-      log.error("Unable to ookup domain class ${params.__newObjectClass}");
-    }
-
-    redirect(url: request.getHeader('referer'))
-  }
-
 }
