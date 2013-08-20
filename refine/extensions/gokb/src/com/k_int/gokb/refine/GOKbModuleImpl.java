055f00097926db7ef610517aeb4ae9c2ce5f205c
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-08-20 14:10:50 +0100

    Module version incremented.

--- refine/extensions/gokb/src/com/k_int/gokb/refine/GOKbModuleImpl.java
+++ refine/extensions/gokb/src/com/k_int/gokb/refine/GOKbModuleImpl.java
@@ -30,7 +30,7 @@ public class GOKbModuleImpl extends ButterflyModuleImpl {
     public static GOKbModuleImpl singleton;
     public static ExtendedProperties properties;
 
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     
     private static String userDetails = null;
     
