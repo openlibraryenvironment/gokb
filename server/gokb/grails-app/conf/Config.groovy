26163f546f71a2bf3eee4a297f6c293702471a67
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-11-12 11:24:39 +0000

    Version incremented to 1.7

--- server/gokb/grails-app/conf/Config.groovy
+++ server/gokb/grails-app/conf/Config.groovy
@@ -25,7 +25,7 @@ identifiers.class_ones = [
 
 project_dir = new java.io.File(org.codehaus.groovy.grails.io.support.GrailsResourceUtils.GRAILS_APP_DIR + "/../project-files/").getCanonicalPath() + "/"
 
-refine_min_version = "1.6"
+refine_min_version = "1.7"
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
