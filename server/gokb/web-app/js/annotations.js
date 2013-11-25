6f3ac294cc78f5fab6cb58b7a03e067b69c0d9cf
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-11-25 15:16:39 +0000

    Added editor functionality to the annotation library.

--- server/gokb/web-app/js/annotations.js
+++ server/gokb/web-app/js/annotations.js
@@ -1,8 +1,94 @@
 (function($) {
   
-  // Add a click even to hide any pop-overs that are visible.
-  $('html').click(function(e) {
-    $('.annotated').popover('hide');
+  function refreshAnnotationPos (annotated, annotation) {
+    
+    var annotated_centre = (annotated.outerHeight() / 2) + annotated.offset().top;
+    var annotation_half = annotation.outerHeight() / 2;
+    
+    var new_top = annotated_centre - annotation_half;
+  
+    // move the element up.
+    annotation.offset({
+      left: annotation.offset().left,
+      top: new_top
+    });
+  }
+  
+  function showHideEditorButtons(editor_el, show) {
+    if (show) {
+      
+      // Add buttons as a sibling of the editor.
+      var wrapper = $('<div />').addClass('editor-buttons').append(
+        $('<button class="btn btn-small btn-success" />').text("Save").click(function() {
+          alert("Saved");
+          editor_el.destroy();
+          showHideEditorButtons(editor_el, false);
+
+          // Shift the element back.
+          var popover = editor_el.closest('.popover');
+          
+          // Refresh the position.
+          refreshAnnotationPos (popover.prev('.annotated'), popover);
+        })
+      );
+      
+      // Append the cancel button.
+      wrapper.append(
+        $('<button class="btn btn-small btn-danger" />').text("Cancel").click(function() {
+          editor_el.destroy();
+          showHideEditorButtons(editor_el, false);
+
+          // Shift the element back.
+          var popover = editor_el.closest('.popover');
+          
+          // Refresh the position.
+          refreshAnnotationPos (popover.prev('.annotated'), popover);
+        })
+      );
+      
+      // Append next to the editor.
+      $(editor_el).parent().append(wrapper);
+      
+    } else {
+      // Hide/remove.
+      $('.editor-buttons', editor_el.parent()).remove();
+    }
+  }
+  
+  // Add a tooltip to each editable annotation to direct the admin user.
+  $('.annotation-editable').tooltip();
+  
+  // Add the double click listener to the HTML. All double clicks will bubble up to here so we need
+  // to be selective on how we respond.
+  $('html').dblclick(function(e) {
+    
+    // Get the element that was double-clicked.
+    var me = $(e.target);
+    
+    // Editable annotation.
+    if (me.hasClass('annotation-editable')) {
+      
+      // Add an editor inline.
+      me.summernote({
+        focus: true,
+        toolbar: [
+          ['style', ['bold', 'italic', 'underline', 'clear']],
+          ['para', ['ul', 'ol']],
+          ['web',['link']]
+        ],
+        oninit: function() {
+          showHideEditorButtons(me, true);
+        }
+      });
+      
+      // Move the popover to keep it inline with it's caller.
+      var the_popover = me.closest('.popover')
+      if (the_popover.length == 1) {
+        
+        // Shift the element.
+        refreshAnnotationPos (the_popover.prev('.annotated'), the_popover);
+      }
+    }
   });
   
   // Need to add the annotations in pop-overs.
@@ -22,12 +108,21 @@
         "html"    : true,
         "trigger" : "manual",
         "title"   : target.text(),
-        "content" : $('<div />').append(annotation.detach().show()).html()
+        "content" : annotation.detach().show()
+        
+      }).on("hidden", function(e) {
+        
+        // Listen to the hidden even and destroy any editor that might exist here.
+        annotation.destroy();
+
+        // Also need to ensure we remove the buttons.
+        showHideEditorButtons(annotation, false);
         
       }).click(function(e) {
         
         // Toggle this element.
-        var current = $(this).popover('toggle');
+        var current = $(this);
+        current.popover('toggle');
         
         // Close the other pop-overs.
         $('.annotated').each(function(){
@@ -37,13 +132,10 @@
         });
         
         // Ensure we stop this even bubbling here so it doesn't trigger the on click,
-        // event registered on the html.
+        // event registered on the HTML.
         e.stopPropagation();
+        
       });
-      
-      // Now remove the annotation from the dom.
-      annotation.remove()
     }
   });
-  
 })(jQuery);
