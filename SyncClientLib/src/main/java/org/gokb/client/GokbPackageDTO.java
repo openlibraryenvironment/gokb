34bf75a347cfacbb61a5ed1b19d408855e369522
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-01-15 11:09:53 +0000

    Added DTO classes

---
+++ SyncClientLib/src/main/java/org/gokb/client/GokbPackageDTO.java
@@ -0,0 +1,8 @@
+package org.gokb.client;
+
+public class GokbPackage {
+
+  String packageId
+  String packageName
+  List tipps
+}
---
+++ SyncClientLib/src/main/java/org/gokb/client/GokbTippDTO.java
@@ -0,0 +1,21 @@
+package org.gokb.client;
+
+public class GokbTipp {
+
+  String tippId
+  String pkgId
+  String pkgName
+  String platId
+  String platName
+  String title
+  String titleId
+  String startVolume
+  String startIssue
+  String startDate
+  String endVolume
+  String endIssue
+  String endDate
+  String coverageDepth
+  String coverageNote
+
+}
