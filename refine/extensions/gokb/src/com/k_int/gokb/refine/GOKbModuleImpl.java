9c82f2ea04e8566cc9aa46fef193d25de1c03606
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-03-19 12:59:09 +0000

    Version increment for the client and server.

--- refine/extensions/gokb/src/com/k_int/gokb/refine/GOKbModuleImpl.java
+++ refine/extensions/gokb/src/com/k_int/gokb/refine/GOKbModuleImpl.java
@@ -33,7 +33,7 @@ public class GOKbModuleImpl extends ButterflyModuleImpl {
     
     private RefineWorkspace[] workspaces;
 
-    public static final String VERSION = "1.7";
+    public static final String VERSION = "2.5";
 
     private static String userDetails = null;
 
