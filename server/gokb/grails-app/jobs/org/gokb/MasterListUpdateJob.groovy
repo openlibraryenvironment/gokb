e898638fd5ecd66c65c162129daf6114895faafb
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-04-04 22:18:55 +0100

    Renamed the Job

---
+++ server/gokb/grails-app/jobs/org/gokb/MasterListUpdateJob.groovy
@@ -0,0 +1,22 @@
+package org.gokb
+
+import org.apache.commons.logging.Log;
+
+class MasterListUpdateJob {
+  
+  // Allow only one run at a time.
+  def concurrent = false
+  
+  static triggers = {
+//      cron name: 'MidnightGeneration', cronExpression: "0 0 0 * * ?", startDelay:60000
+    cron name: 'MasterListUpdateTrigger', cronExpression: "0/15 * * * * ?", startDelay:10000
+  }
+  
+  PackageService packageService
+
+  def execute() {
+    log.debug ("Beginning scheduled Master Package update job.") 
+    packageService.updateAllMasters(true)
+    log.debug ("Master Package update job completed.")
+  }
+}
