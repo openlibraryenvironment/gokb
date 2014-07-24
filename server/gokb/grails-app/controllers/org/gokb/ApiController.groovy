122bb2705a53bd1dfaa29628eb3925f1e47efb75
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-07-24 11:22:41 +0100

    Fixes #122

--- server/gokb/grails-app/controllers/org/gokb/ApiController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/ApiController.groovy
@@ -701,7 +701,9 @@ class ApiController {
         } 
       }
       
-      def formattedResults = results.collect { KBComponent comp ->
+      // SO: listDistinct will not work with pagination, so we are forcing a linked HashSet here which will maintain the order from the
+      // the query but strip out the duplicates.
+      LinkedHashSet formattedResults = results.collect { KBComponent comp ->
             
         // Add each requested parameter to the return map. Label is a special case as we return "name"
         // for this. This is to keep backwards compatibility with the JQuery autocomplete default behaviour.
