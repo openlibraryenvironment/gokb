bda3a02756331c40a39155191fd04220a7cc9c4f
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-25 10:32:01 +0100

    scripts seem to be workgin

--- refine/extensions/makeall.sh
+++ refine/extensions/makeall.sh
@@ -1,12 +1,13 @@
 
 echo run from gokb-phase1/refine/extensions
 
-for config in 'gokb_live_kuali gokb_test_kint gokb_test_kuali'
+for config in gokb_live_kuali gokb_test_kint gokb_test_kuali
 do
   cd gokb
   cp module/MOD-INF/$config.properties gokb.properties
   ant -Drefine.dir=/home/ibbo/google-refine-2.5 -Dserver.dir=/home/ibbo/apache-tomcat
   cd ..
   zip -r $config ./gokb
+  echo completed build for $config
 done
 
