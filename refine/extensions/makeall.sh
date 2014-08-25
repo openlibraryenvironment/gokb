1da1158f306d4dfd91eef007acca7feef5d216b6
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-25 15:30:55 +0100

    updated extensions

--- refine/extensions/makeall.sh
+++ refine/extensions/makeall.sh
@@ -4,7 +4,7 @@ echo run from gokb-phase1/refine/extensions
 for config in gokb_live_kuali gokb_test_kint gokb_test_kuali
 do
   cd gokb
-  cp module/MOD-INF/$config.properties gokb.properties
+  cp module/MOD-INF/$config.properties module/MOD-INF/gokb.properties
   grep "^api.entry" module/MOD-INF/$config.properties
   ant -Drefine.dir=/home/ibbo/google-refine-2.5 -Dserver.dir=/home/ibbo/apache-tomcat
   cd ..
