9bbf2b319b559fdf286ad4afc83dfc39cc4556f2
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-25 16:05:25 +0100

    remove debug

--- server/gokb/grails-app/controllers/org/gokb/ResourceController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/ResourceController.groovy
@@ -24,12 +24,9 @@ class ResourceController {
     def result = [:]
 
     if ( params.id ) {
-      log.debug("Attempt to retrieve ${params.id} and find a global template for it");
       result.displayobj = genericOIDService.resolveOID(params.id)
 
       if ( result.displayobj ) {
-
-        log.debug("Got object");
 
         def new_history_entry = new History(controller:params.controller,
             action:params.action,
@@ -37,28 +34,21 @@ class ResourceController {
             owner:user,
             title:"View ${result.displayobj.toString()}").save()
 
-        log.debug("i2");
-
         result.displayobjclassname = result.displayobj.class.name
         result.__oid = "${result.displayobjclassname}:${result.displayobj.id}"
         result.displaytemplate = grailsApplication.config.globalDisplayTemplates[result.displayobjclassname]
 
-        log.debug("i3");
         // Add any refdata property names for this class to the result.
         result.refdata_properties = classExaminationService.getRefdataPropertyNames(result.displayobjclassname)
         result.displayobjclassname_short = result.displayobj.class.simpleName
 
-        log.debug("component test")
-
         result.isComponent = (result.displayobj instanceof KBComponent)
         
-        log.debug("ACL");
         result.acl = gokbAclService.readAclSilently(result.displayobj)
       }
       else {
         log.debug("unable to resolve object");
       }
-      log.debug("done")
     }
     result
   }
