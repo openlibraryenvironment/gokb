6d7d0c4cfe3f7f241c529a90bc58a5253af8cfdf
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-09-03 13:32:08 +0100

    Fixed issue with removal from a list that is persisted using a combo.

--- server/gokb/src/groovy/org/gokb/ComboPersistedList.groovy
+++ server/gokb/src/groovy/org/gokb/ComboPersistedList.groovy
@@ -9,12 +9,12 @@ import org.gokb.cred.KBComponent;
 import org.gokb.cred.RefdataValue;
 
 public class ComboPersistedList extends org.apache.commons.collections.list.AbstractListDecorator {
-  
+
   private final KBComponent component;
   private final boolean incoming;
   private final RefdataValue status;
   private final RefdataValue type;
-  
+
   public ComboPersistedList (KBComponent component, RefdataValue status, RefdataValue type, Collection<Combo> vals) {
     this (component, status, type, vals, false );
   }
@@ -26,89 +26,93 @@ public class ComboPersistedList extends org.apache.commons.collections.list.Abst
     this.type = type;
     this.incoming = incoming;
   }
-  
+
   @Override
   public boolean add(Object element) {
-    
+
     KBComponent comp = (KBComponent)element;
-    
+
     // Try and add the object.
     boolean added = super.add(element);
-    
+
     if (added) {
-      
+
       // Create the Combo.
       Combo combo = new Combo (
-		status 		: (status),
-		type 		: (type),
-		startDate 	: new Date()
-	  )
-	  
+          status 		: (status),
+          type 		: (type),
+          startDate 	: new Date()
+          )
+
       // Add to the 2 comboLists.
       if (incoming) {
-        
+
         // Incoming combos of component.
         component.addToIncomingCombos(combo)
         comp.addToOutgoingCombos(combo)
-        
+
       } else {
-        
+
         // Outgoing combos of component.
         component.addToOutgoingCombos (combo);
         comp.addToIncomingCombos (combo);
       }
     }
-    
+
     return added;
   }
 
   @Override
   public boolean addAll( Collection coll) {
-    
+
     // Ensure we use our extended add.
     boolean added = true;
     for (Object element : coll) {
       added = added && this.add(element);
     }
-    
+
     return added;
   }
 
   @Override
   public boolean remove(Object element) {
-    
+
     KBComponent comp = (KBComponent)element;
-    
+
     // Removed item successfully.
     boolean removed = super.remove(comp);
     if (removed) {
-      
+
       // Create the Combo.
       Combo combo = new Combo ();
       combo.setStatus(status);
       combo.setType(type);
-      
+
       // Add the from/to components before removing from the 2 lists.
       if (incoming) {
         
+        
+
         combo.setFromComponent(comp);
         combo.setToComponent(component);
-        
-        // Incoming combos of component.
-        component.getIncomingCombos().remove (combo);
-        comp.getOutgoingCombos().remove (combo);
-        
+
       } else {
-      
+
         combo.setFromComponent(component);
         combo.setToComponent(comp);
-        
-        // Outgoing combos of component.
-        comp.getIncomingCombos().remove (combo);
-        component.getOutgoingCombos().remove (combo);
       }
+      
+      // Remove the combos.
+      Combo.createCriteria().list {
+        and {
+          eq("status", combo.status)
+          eq("type", combo.type)
+          eq("fromComponent", combo.fromComponent)
+          eq("toComponent", combo.toComponent)
+        }
+      }*.delete()
     }
-    
+
     return removed;
   }
 
@@ -119,13 +123,13 @@ public class ComboPersistedList extends org.apache.commons.collections.list.Abst
     for (Object element : coll) {
       removed = removed && this.remove(element);
     }
-    
+
     return removed;
   }
 
   @Override
   public boolean retainAll(Collection coll) {
-    
+
     boolean removed = true;
 
     // Remove all items not in the supplied collection.
@@ -138,7 +142,7 @@ public class ComboPersistedList extends org.apache.commons.collections.list.Abst
         removed = removed && this.remove(element);
       }
     }
-    
+
     return removed;
   }
 }
