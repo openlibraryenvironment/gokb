bc70e901f783fc3ed93341ec5c22b563467f1066
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-09-10 15:06:48 +0100

    Initial commit of move over to asset pipeline.

--- server/gokb/grails-app/conf/BuildConfig.groovy
+++ server/gokb/grails-app/conf/BuildConfig.groovy
@@ -89,13 +89,26 @@ grails.project.dependency.resolution = {
       
       runtime ':hibernate:3.6.10.14'
       runtime ':database-migration:1.4.0'
-      runtime ':hibernate:3.6.10.14'
-      runtime ':database-migration:1.4.0'
       
       /*************************************/
       
       runtime ":jquery:1.8.3"
       runtime ':jquery-ui:1.8.24'
+      
+//      runtime ":resources:1.2"
+//      runtime ':gsp-resources:0.4.4'
+
+      // Uncomment these (or add new ones) to enable additional resources capabilities
+      //runtime ":zipped-resources:1.0"
+      //runtime ":cached-resources:1.0"
+      //runtime ":yui-minify-resources:0.1.4"
+
+//      build ':tomcat:7.0.40.1'
+      
+
+//      runtime ":database-migration:1.3.3"
+
+//      compile ':cache:1.0.1'
 	
     	// Joda time to handle the ISO dates.
     	compile ":joda-time:1.4"
