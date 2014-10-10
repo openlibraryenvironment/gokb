f56e24ee4f0971b8563c0dd1250afe8f17daf405
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-10 11:18:32 +0100

    Added ldPublish

---
+++ linkedData/ldPublish/orgs.groovy
@@ -0,0 +1,34 @@
+@Grapes([
+    // @GrabResolver(name='central', root='http://central.maven.org/maven2/'),
+    @Grab(group='org.slf4j', module='slf4j-api', version='1.7.6'),
+    @Grab(group='org.slf4j', module='jcl-over-slf4j', version='1.7.6'),
+    @Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
+    @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1'),
+    @Grab(group='xerces', module='xercesImpl', version='2.9.1'),
+    @Grab(group='org.apache.jena', module='jena-tdb', version='1.0.2'),
+    @Grab(group='org.apache.jena', module='jena-core', version='2.11.2'),
+    @Grab(group='org.apache.jena', module='jena-arq', version='2.11.2'),
+    @Grab(group='org.apache.jena', module='jena-iri', version='1.0.2'),
+    @Grab(group='org.apache.jena', module='jena-spatial', version='1.0.1'),
+    @Grab(group='org.apache.jena', module='jena-security', version='2.11.2'),
+    @Grab(group='org.apache.jena', module='jena-text', version='1.0.1'),
+    @Grab(group='virtuoso', module='virtjena', version='2'),
+    @Grab(group='virtuoso', module='virtjdbc', version='4.1')
+])
+
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.URLENC
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+import groovyx.net.http.*
+import org.apache.http.entity.mime.*
+import org.apache.http.entity.mime.content.*
+import java.nio.charset.Charset
+import static groovy.json.JsonOutput.*
+import virtuoso.jena.driver.*;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.* ;
+import com.hp.hpl.jena.graph.*;
+import java.text.SimpleDateFormat
+
+print "Use OAI to get latest orgs data and publish as linked open data"
