5556108dc359602e81846e9a2d8e77ad9f034ce3
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-29 09:54:07 +0000

    Started thinking about transitioning to an architecture where we select the values for columns rather than construct them

--- server/gokb/src/groovy/com/k_int/HQLBuilder.groovy
+++ server/gokb/src/groovy/com/k_int/HQLBuilder.groovy
@@ -44,7 +44,13 @@ public class HQLBuilder {
    *
    *
    */
-  public static def build(grailsApplication, qbetemplate, params, result, target_class, genericOIDService) {
+  public static def build(grailsApplication, 
+                          qbetemplate, 
+                          params,
+                          result, 
+                          target_class, 
+                          genericOIDService,
+                          returnObjectsOrRows='objects') {
     // select o from Clazz as o where 
 
     // log.debug("build ${params}");
@@ -105,7 +111,13 @@ public class HQLBuilder {
     // log.debug("BindVars: ${hql_builder_context.bindvars}");
 
     def count_hql = "select count (o) ${hql}"
-    def fetch_hql = "select o ${hql}"
+    def fetch_hql = null
+    if ( returnObjectsOrRows=='objects' ) {
+      fetch_hql = "select o ${hql}"
+    }
+    else {
+      fetch_hql = "select ${buildFieldList(qbetemplate.qbeConfig.qbeResults)} ${hql}"
+    }
 
     log.debug("Attempt count qry ${count_hql}");
     // log.debug("Attempt qry ${fetch_hql}");
@@ -301,4 +313,14 @@ public class HQLBuilder {
     sw.toString();
   }
 
+  static def buildFieldList(defns) {
+    def result = new java.io.StringWriter()
+    result.write('o.id');
+    result.write(',o.class');
+    defns.each { defn ->
+      result.write(",o.");
+      result.write(defn.property);
+    }
+    result.toString();
+  }
 }
