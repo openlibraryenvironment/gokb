3ff6007d21f2bd0db370c45915dfdd49761c1640
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-11-25 10:14:15 +0000

    Removed legacy menu generation.

--- server/gokb/grails-app/conf/UserDetailsFilters.groovy
+++ server/gokb/grails-app/conf/UserDetailsFilters.groovy
@@ -98,64 +98,6 @@ class UserDetailsFilters {
           if ( session.userPereferences == null ) {
             //log.debug("Set up user prefs");
             session.userPereferences = request.user.getUserPreferences()
-            // Generate Menu for this user.
-            session.userPereferences.mainMenuSections = [:]
-            session.userPereferences.createMenu = []
-            
-            def current_type = null
-            def current_list = null;
-            // Step 1 : List all domains available to this user order by type, grouped into type
-            
-            domains = KBDomainInfo.createCriteria().list {
-              
-              ilike ('dcName', 'org.gokb%')
-              
-              createAlias ("type", "menueType")
-              
-              order ('menueType.sortKey','asc')
-              order ('dcSortOrder','asc')
-              order ('displayName','asc')
-            }
-            
-            domains.each { d ->
-              // log.debug("Process ${d.displayName} (${d.dcSortOrder}) - ${d.type.id}");
-              if ( d.type.id != current_type ) {
-                current_type = d.type.id
-                // current_list = [:]
-                current_list = []
-                session.userPereferences.mainMenuSections.put(d.type.value, current_list)
-                //log.debug("Added new menu section for ${d.type.value}");
-              }
-
-              // Get the target class.
-              Class tc = Class.forName(d.dcName)
-              
-              // boolean hasPermission(Authentication authentication, domainObject, Permission… permissions)
-              if ( tc.isTypeReadable() ) { // || request.request.isUserInRole('ROLE_ADM')) {
- 
-                // Find any searches for that domain that the user has access to and add them to the menu section
-                def searches_for_this_domain = grailsApplication.config.globalSearchTemplates.findAll{it.value.baseclass==d.dcName}
-                searches_for_this_domain.each {
-                  //log.debug("Adding search for ${it.key} - ${it.value.baseclass}");
-                  current_list.add(it) // [it.key] = it.value
-                }
-              }
-
-              // Add if creatable.
-              if ( tc.isTypeCreatable() ) { // || request.request.isUserInRole('ROLE_ADM') ) {
-                
-                // SO: Commenting out for now and overridden isTypeCreatable() on the TitleInstancePackagePlatform class to return false
-                
-                // II: This is dirty, but it will do for now
-                if ( d.dcName == 'org.gokb.cred.TitleInstancePackagePlatform' ) {
-                  // Suppress for now.
-                  log.error ("TitleInstancePackagePlatform.isTypeCreatable() is testing true!!")
-                }
-//                else {
-                  session.userPereferences.createMenu.add(d);
-//                }
-              }
-            }
           }
         }
       }
