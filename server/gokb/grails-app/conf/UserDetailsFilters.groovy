5426910300d10e1bf31956929394c12bc7a114fd
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-11-26 16:52:52 +0000

    Forgot to check session var first.

--- server/gokb/grails-app/conf/UserDetailsFilters.groovy
+++ server/gokb/grails-app/conf/UserDetailsFilters.groovy
@@ -35,61 +35,64 @@ class UserDetailsFilters {
           request.user = User.get(springSecurityService.principal.id)
           request.userOptions = request.user.getUserOptions()
           
-          // Map to hold the menus.
-          final Map <String, LinkedHashMap<String, Map>> menus = new HashMap <String, LinkedHashMap<String, Map>>().withDefault {
-            new LinkedHashMap<String, Map>().withDefault {
-              new ArrayList()
+          if (!session.menus) {
+          
+            // Map to hold the menus.
+            final Map <String, LinkedHashMap<String, Map>> menus = new HashMap <String, LinkedHashMap<String, Map>>().withDefault {
+              new LinkedHashMap<String, Map>().withDefault {
+                new ArrayList()
+              }
             }
-          }
-          
-          // Add to the session.
-          session.menus = menus
-          
-          // Step 1 : List all domains available to this user order by type, grouped into type
-          def domains = KBDomainInfo.createCriteria().list {
             
-            ilike ('dcName', 'org.gokb%')
+            // Add to the session.
+            session.menus = menus
             
-            createAlias ("type", "menueType")
-            
-            order ('menueType.sortKey','asc')
-            order ('menueType.value','asc')
-            order ('dcSortOrder','asc')
-            order ('displayName','asc')
-          }
-          
-          domains.each { d ->
-    
-            // Get the target class.
-            Class tc = Class.forName(d.dcName)
-            
-            if ( tc.isTypeReadable() ) {
-    
-              // Find any searches for that domain that the user has access to and add them to the menu section
-              def searches_for_this_domain = grailsApplication.config.globalSearchTemplates.findAll{it.value.baseclass==d.dcName}
+            // Step 1 : List all domains available to this user order by type, grouped into type
+            def domains = KBDomainInfo.createCriteria().list {
               
-              searches_for_this_domain.each { key, val ->
+              ilike ('dcName', 'org.gokb%')
+              
+              createAlias ("type", "menueType")
+              
+              order ('menueType.sortKey','asc')
+              order ('menueType.value','asc')
+              order ('dcSortOrder','asc')
+              order ('displayName','asc')
+            }
+            
+            domains.each { d ->
+      
+              // Get the target class.
+              Class tc = Class.forName(d.dcName)
+              
+              if ( tc.isTypeReadable() ) {
+      
+                // Find any searches for that domain that the user has access to and add them to the menu section
+                def searches_for_this_domain = grailsApplication.config.globalSearchTemplates.findAll{it.value.baseclass==d.dcName}
                 
-                // Add a menu item.
-                menus["search"]["${d.type.value}"] << [
-                  text : val.title,
-                  link : ['controller' : 'search', 'action' : 'index', 'params' : [qbe:'g:'+ key]],
-                  attr : ['title' : "Search ${val.title}"]
+                searches_for_this_domain.each { key, val ->
+                  
+                  // Add a menu item.
+                  menus["search"]["${d.type.value}"] << [
+                    text : val.title,
+                    link : ['controller' : 'search', 'action' : 'index', 'params' : [qbe:'g:'+ key]],
+                    attr : ['title' : "Search ${val.title}"]
+                  ]
+                }
+              }
+      
+              // Add if creatable.
+              if ( tc.isTypeCreatable() ) { 
+                if ( d.dcName == 'org.gokb.cred.TitleInstancePackagePlatform' ) {
+                  // Suppress for now.
+                  log.error ("TitleInstancePackagePlatform.isTypeCreatable() is testing true!!")
+                }
+                menus["create"]["${d.type.value}"] << [
+                  text : d.displayName,
+                  link : ['controller' : 'create', 'action' : 'index', 'params' : [tmpl:d.dcName]],
+                  attr : ['title' : "New ${d.displayName}"]
                 ]
               }
-            }
-    
-            // Add if creatable.
-            if ( tc.isTypeCreatable() ) { 
-              if ( d.dcName == 'org.gokb.cred.TitleInstancePackagePlatform' ) {
-                // Suppress for now.
-                log.error ("TitleInstancePackagePlatform.isTypeCreatable() is testing true!!")
-              }
-              menus["create"]["${d.type.value}"] << [
-                text : d.displayName,
-                link : ['controller' : 'create', 'action' : 'index', 'params' : [tmpl:d.dcName]],
-                attr : ['title' : "New ${d.displayName}"]
-              ]
             }
           }
 
