331b48d0b7ef527799c1579cc0044f1af491a86c
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-01-29 12:30:43 +0000

    Back to normname search for packages

--- server/gokb/grails-app/conf/Config.groovy
+++ server/gokb/grails-app/conf/Config.groovy
@@ -516,7 +516,7 @@ globalSearchTemplates = [
           prompt:'Name of Package',
           qparam:'qp_name',
           placeholder:'Package Name',
-          contextTree:['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'name', 'wildcard':'B', normalise:false]
+          contextTree:['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'normname', 'wildcard':'B', normalise:true]
         ]
       ],
       qbeGlobals:[
