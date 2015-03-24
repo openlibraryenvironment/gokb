6c45e11d8a9ba3be3cc263a27216c09a25824bb1
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-03-24 15:00:42 +0000

    Lookup group for group page

--- server/gokb/grails-app/controllers/org/gokb/GroupController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/GroupController.groovy
@@ -1,6 +1,26 @@
 package org.gokb
 
+import grails.plugins.springsecurity.Secured
+import grails.util.GrailsNameUtils
+
+import org.gokb.cred.*
+import org.hibernate.SessionFactory;
+import org.hibernate.transform.AliasToEntityMapResultTransformer
+
+import org.gokb.cred.CuratoryGroup
+
+
 class GroupController {
 
-    def index() { }
+  def grailsApplication
+  def springSecurityService
+
+  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
+  def index() { 
+    def result = [:]
+    if ( params.id ) {
+      result.group = CuratoryGroup.get(params.id);
+    }
+    return result
+  }
 }
--- server/gokb/grails-app/views/group/index.gsp
+++ server/gokb/grails-app/views/group/index.gsp
@@ -5,5 +5,12 @@
 <title>GOKb: Group</title>
 </head>
 <body>
+  <h1 class="page-header">${group?.name} Curated Packages</h1>
+  <div id="mainarea" class="panel panel-default">
+  </div>
+
+  <h1 class="page-header">${group?.name} Review Tasks</h1>
+  <div id="mainarea" class="panel panel-default">
+  </div>
 </body>
 </html>
