5527e75f7829dbe6b849a4a9ce0b769e4b7b52ce
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-03-25 11:39:12 +0000

    Working query for CG packages

--- server/gokb/grails-app/controllers/org/gokb/GroupController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/GroupController.groovy
@@ -7,7 +7,7 @@ import org.gokb.cred.*
 import org.hibernate.SessionFactory;
 import org.hibernate.transform.AliasToEntityMapResultTransformer
 
-import org.gokb.cred.CuratoryGroup
+import org.gokb.cred.*
 
 
 class GroupController {
@@ -20,6 +20,15 @@ class GroupController {
     def result = [:]
     if ( params.id ) {
       result.group = CuratoryGroup.get(params.id);
+
+      // get review tasks for this CG [max 20]
+      // def cg_review_tasks_hql = " from ReviewRequest as rr join rr.incomingCombos where 1==2"
+
+      // get packages for this CG [max 20]
+      def cg_packages_hql = " from Package as p where exists ( select c from p.outgoingCombos as c where c.toComponent = ? and c.type.value = 'Package.CuratoryGroups')"
+
+      result.package_count = Package.executeQuery('select count(p) '+cg_packages_hql,[result.group]);
+      result.packages = Package.executeQuery('select p '+cg_packages_hql,[result.group],[max: 20, offset: 0]);
     }
     return result
   }
--- server/gokb/grails-app/views/group/index.gsp
+++ server/gokb/grails-app/views/group/index.gsp
@@ -7,10 +7,40 @@
 <body>
   <h1 class="page-header">${group?.name} Curated Packages</h1>
   <div id="mainarea" class="panel panel-default">
+
+    <div class="pull-right">Packages 1 to ${Math.min(20,package_count[0])} of ${package_count[0]}</div>&nbsp;<br/>
+
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <td>Package Name</td>
+        </tr>
+      </thead>
+      <tbody>
+        <g:each in="${packages}" var="pkg">
+          <tr>
+            <td>${pkg.name}</td>
+          </tr>
+        </g:each>
+      </tbody>
+    </table>
   </div>
 
   <h1 class="page-header">${group?.name} Review Tasks</h1>
   <div id="mainarea" class="panel panel-default">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <td></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+
 </body>
 </html>
