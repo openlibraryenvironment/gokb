89593f661bc770b23824e79681beb856443dafe4
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-03-25 14:58:35 +0000

    updated

--- server/gokb/grails-app/controllers/org/gokb/GroupController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/GroupController.groovy
@@ -21,8 +21,12 @@ class GroupController {
     if ( params.id ) {
       result.group = CuratoryGroup.get(params.id);
 
-      // get review tasks for this CG [max 20]
-      // def cg_review_tasks_hql = " from ReviewRequest as rr join rr.incomingCombos where 1==2"
+      // get review tasks for users attached to this CG [max 20]
+      // def cg_review_tasks_hql = " from ReviewRequest as rr where exists ( select g from CuratoryGroup as cg where 
+      def cg_review_tasks_hql = " from ReviewRequest as rr where allocatedTo in ( select u from CuratoryGroup as cg join cg.users as u where cg = ? )"
+      result.rr_count = Package.executeQuery('select count(rr) '+cg_review_tasks_hql,[result.group]);
+      result.rrs = Package.executeQuery('select rr '+cg_review_tasks_hql,[result.group],[max: 20, offset: 0]);
+
 
       // get packages for this CG [max 20]
       def cg_packages_hql = " from Package as p where exists ( select c from p.outgoingCombos as c where c.toComponent = ? and c.type.value = 'Package.CuratoryGroups')"
