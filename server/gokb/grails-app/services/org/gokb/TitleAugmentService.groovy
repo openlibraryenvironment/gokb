3a7bf1c3cf66af0512c8d19f045b83d55d1ad00e
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-05-18 16:05:23 +0100

    Playing with crossref api

--- server/gokb/grails-app/services/org/gokb/TitleAugmentService.groovy
+++ server/gokb/grails-app/services/org/gokb/TitleAugmentService.groovy
@@ -3,13 +3,24 @@ package org.gokb
 import org.gokb.refine.*;
 import org.gokb.cred.*;
 
+import groovy.util.slurpersupport.GPathResult
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+import groovyx.net.http.*
+import org.apache.http.entity.mime.*
+import org.apache.http.entity.mime.content.*
+import java.nio.charset.Charset
+import org.apache.http.*
+import org.apache.http.protocol.*
+
+
 
 class TitleAugmentService {
 
   def grailsApplication
   def edinaPublicationsAPIService
 
-    def augment(titleInstance) {
+  def augment(titleInstance) {
       // If the title does have a suncat-id
       if ( titleInstance.getIdentifierValue('SUNCAT' ) == null ) {
         def lookupResult = edinaPublicationsAPIService.lookup(titleInstance.name)
@@ -43,6 +54,62 @@ class TitleAugmentService {
         else {
         }
       }
+  }
+
+  def doEnrichment() {
+    // Iterate though titles touched since last cursor
+    // def title_ids = TitleInstance.executeQuery("select id from TitleInstance");
+    // title_ids.each { ti_id ->
+    //   def title = TitleInstance.get(ti_id);
+    //   console.log("Process ${ti_id} ${title.name}");
+    //   synchronized(this) {
+    //     this.sleep(1000);
+    //   }
+    // }
+    crossrefSync();
+  }
+
+  def crossrefSync() {
+    def endpoint = 'http://api.crossref.org';
+    def target_service = new RESTClient(endpoint)
+    def offset = 0;
+    def num_processed = 1;
+
+    while ( num_processed > 0 ) {
+      num_processed = 0;
+      try {
+        target_service.request(GET) { request ->
+          uri.path='/journals'
+          uri.query = [
+            offset:offset,
+            rows:100
+          ]
+  
+          response.success = { resp, data ->
+            data.message.items.each { item ->
+              log.debug("item:: ${item.title}");
+              offset++;
+            }
+            num_processed = data.message.'total-results'
+          }
+          response.failure = { resp ->
+            log.error("Error - ${resp}");
+          }
+        }
+      }
+      catch ( Exception e ) {
+        e.printStackTrace();
+      }
+      finally {
+      }
+
+      synchronized(this) {
+        Thread.sleep(5000);
+      }
     }
 
+
+    
+    // {"status":"ok","message-type":"journal-list","message-version":"1.0.0","message":{"items":[],"total-results":39790,"query":{"search-terms":null,"start-index":70000},"items-per-page":20}}
+  }
 }
