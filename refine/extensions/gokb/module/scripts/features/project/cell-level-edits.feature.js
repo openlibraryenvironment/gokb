1a0e5422d9c57b158cdffdebfd2bf7fe4aa592b7
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2015-09-14 19:34:25 +0100

    Added prepending of events extension to jQuery and added handlers to existing buttons.

--- refine/extensions/gokb/module/scripts/features/project/cell-level-edits.feature.js
+++ refine/extensions/gokb/module/scripts/features/project/cell-level-edits.feature.js
@@ -12,41 +12,56 @@
       // Extend the protoype of the CellUI and add a new method for capturing the cell level edits.
       DataTableCellUI.prototype._extendEdit = function(elmt) {
         
-        // Create the capture checkbox.
-        var capture_check = $('<input type="checkbox" name="capture-edit" id="capture-edit" value="true" />');
-        
         // Grab the editor and add the extra checkbox.
         var editor = $(".data-table-cell-editor")
           .append(
             $('<div class="data-table-cell-editor-action" />')
-              .append(capture_check)
+              .append($('<input bind="captureCheck" type="checkbox" name="capture-edit" id="capture-edit" value="true" />'))
               .append($('<label for="capture-edit" />').text("Capture Edit"))
-              .append($('<div class="data-table-cell-editor-key" bind="or_views_ctrlC">Ctrl+C</div>'))
+              .append($('<div class="data-table-cell-editor-key" bind="or_views_ctrl1">Ctrl+1</div>'))
           )
         ;
         
-        // Grab the current click events for the buttons.
-        var okAllClicks = $._data( elmts.okallButton.get(0) );
+        // Bind the elements.
+        var elmts = DOM.bind(editor);
+        
+        // The variable that holds the checked value.
+        var capture = false;
         
         // Add the change listener so we can show and hide the table of columns for the condition.
-        capture_check.change(function(e){
+        elmts.captureCheck.change(function(e){
           
           // The check box.
           var me = $(this);
           
           // Checked value.
-          var checked = me.prop("checked");
-          
-          if (checked) {
-          } else {
+          capture = me.prop("checked");
+        });
+        
+        elmts.okButton.prependEvent ('click', function(e) {          
+          if (capture) {
+            // Run the custom capture of single cell edit.
+            alert ("Single cell capture.");
+            e.stopImmediatePropagation();
           }
         });
         
-        // Bind the elements.
-        var elmts = DOM.bind(editor);
+        elmts.okallButton.prependEvent ('click', function(e) {          
+          if (capture) {
+            // Run the custom capture of single cell edit.
+            alert ("Multiple cell capture.");
+            e.stopImmediatePropagation();
+          }
+        });
         
-        
-        
+        // Also... We should add a key event handler for our new capture element.
+        elmts.textarea
+          .keydown(function(evt) {
+            if (!evt.shiftKey && evt.ctrlKey && evt.keyCode == 49) {
+              elmts.captureCheck.trigger('click');
+            }
+          })
+        ;
       }
       
       // Extend the original function call and call our new method afterwards.
