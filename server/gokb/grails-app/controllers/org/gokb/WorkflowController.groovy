fcf90476a1f7b39e161cee6993d7a4568f6c8320
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-09-24 12:14:00 +0100

    Added blank columns for non-populated KBART2 fields. closes #424

--- server/gokb/grails-app/controllers/org/gokb/WorkflowController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/WorkflowController.groovy
@@ -1142,7 +1142,7 @@ class WorkflowController {
 
           // As per spec header at top of file / section
           // II: Need to add in preceding_publication_title_id
-          writer.write('publication_title\tprint_identifier\tonline_identifier\tdate_first_issue_online\tnum_first_vol_online\tnum_first_issue_online\tdate_last_issue_online\tnum_last_vol_online\tnum_last_issue_online\ttitle_url\tfirst_author\ttitle_id\tembargo_info\tcoverage_depth\tcoverage_notes\tpublisher_name\tpreceding_publication_title_id\n');
+          writer.write('publication_title\tprint_identifier\tonline_identifier\tdate_first_issue_online\tnum_first_vol_online\tnum_first_issue_online\tdate_last_issue_online\tnum_last_vol_online\tnum_last_issue_online\ttitle_url\tfirst_author\ttitle_id\tembargo_info\tcoverage_depth\tcoverage_notes\tpublisher_name\tpreceding_publication_title_id\tdate_monograph_published_print\tdate_monograph_published_online\tmonograph_volume\tmonograph_edition\tfirst_editor\tparent_publication_title_id\n');
 
           def tipps = TitleInstancePackagePlatform.executeQuery(
                          'select tipp.id from TitleInstancePackagePlatform as tipp, Combo as c where c.fromComponent=? and c.toComponent=tipp  and tipp.status.value <> ? and c.type.value = ? order by tipp.id',
@@ -1169,7 +1169,13 @@ class WorkflowController {
                           sanitize( tipp.coverageDepth ) + '\t' +
                           sanitize( tipp.coverageNote ) + '\t' +
                           sanitize( tipp.title.getCurrentPublisher()?.name ) + '\t' +
-                          sanitize( tipp.title.getPrecedingTitleId() )+
+                          sanitize( tipp.title.getPrecedingTitleId() + '\t' )+
+                          '\t' +  // date_monograph_published_print
+                          '\t' +  // date_monograph_published_online
+                          '\t' +  // monograph_volume
+                          '\t' +  // monograph_edition
+                          '\t' +  // first_editor
+                          '\t' +  // parent_publication_title_id
                           '\n');
             tipp.discard();
           }
