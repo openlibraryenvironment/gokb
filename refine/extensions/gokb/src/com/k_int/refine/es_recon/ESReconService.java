01e7bdcef1a6c2b4015a4aa05e57acf6279502d6
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2015-10-26 18:11:20 +0000

    WIP: more work on recon service.

--- refine/extensions/gokb/src/com/k_int/refine/es_recon/ESReconService.java
+++ refine/extensions/gokb/src/com/k_int/refine/es_recon/ESReconService.java
@@ -131,9 +131,9 @@ public class ESReconService {
         ESReconJob currentJob = (ESReconJob) jobs.get(i);
         
         if (!obj.get("error").getAsBoolean()){  
-          results.add( buildRecon(esReconcileConfig, judgmentHistoryEntry, currentJob.getQuery(), obj) );
+          results.add( buildRecon(esReconcileConfig, judgmentHistoryEntry, currentJob.getQuery(), obj ) );
         } else {
-          results.add( buildRecon(esReconcileConfig, judgmentHistoryEntry, currentJob.getQuery(), null) );
+          results.add( buildRecon(esReconcileConfig, judgmentHistoryEntry, currentJob.getQuery(), null ) );
         }
       }
     } catch (IOException e) {
@@ -158,19 +158,28 @@ public class ESReconService {
 
       JsonArray hits = res.getAsJsonObject("hits").getAsJsonArray("hits");
       
+      // Grab the max score here.
+      final float max_score = res.getAsJsonObject("hits").get("max_score").getAsFloat();
+      
       for (int i = 0; i<hits.size(); i++) {
         
         JsonObject hit = hits.get(i).getAsJsonObject();
 
         String type = hit.get("_type").getAsString();
         JsonObject source = hit.getAsJsonObject("_source");
+        
+        // The resource name.
+        String name = source.get("name").getAsString();
+        
+        // The Levenshtein distance.
+        int l_distance = StringUtils.getLevenshteinDistance(StringUtils.lowerCase(text), StringUtils.lowerCase(name));
 
         // Create a recon candidate.
         ReconCandidate candidate = new ReconCandidate(
             hit.get("_id").getAsString(),
-            source.get("name").getAsString(),
+            name,
             new String[]{ type },
-            hit.get("_score").getAsDouble()
+            compatibleScore (max_score, hit.get("_score").getAsFloat(), l_distance)
         );
 
         // Add the candidate for this row.
@@ -179,7 +188,7 @@ public class ESReconService {
         if (i == 0) {
           // First object is the BEST match.
           recon.setFeature(Recon.Feature_nameMatch, text.equalsIgnoreCase(candidate.name));
-          recon.setFeature(Recon.Feature_nameLevenshtein, StringUtils.getLevenshteinDistance(StringUtils.lowerCase(text), StringUtils.lowerCase(candidate.name)));
+          recon.setFeature(Recon.Feature_nameLevenshtein, l_distance);
 
           // TODO: SO - For now the type is always supplied and therefore filtered and will always be matched. 
           recon.setFeature(Recon.Feature_typeMatch, true);
@@ -194,6 +203,28 @@ public class ESReconService {
     //    recon.judgmentAction = "auto";
 
     return recon;
+  }
+  
+  private float compatibleScore (float es_max_score, float es_doc_score, int levenshtein_distance) {
+        
+    // Default to the doc score over the max.
+    float new_score = (es_doc_score/es_max_score);
+    
+    // We don't want to treat strings that differ in any way as 100% certain matches.
+    // So we use the distance measure to offset the results slightly 
+    if (levenshtein_distance > 0) {
+      
+      // Cap it at 99 to allow a very small score.
+      if (levenshtein_distance > 99) {
+        levenshtein_distance = 99;
+      }
+      
+      // Apply a small offset to the results.
+      new_score = new_score - (float)(levenshtein_distance / 100f);
+    }
+    
+    // The new elastic search score
+    return (new_score * 100f);
   }
 
   private Search buildReconSearch (ESReconJob rj) {
