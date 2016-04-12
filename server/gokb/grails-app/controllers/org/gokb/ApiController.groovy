8d377a1e1e5d6a052bd50525509aad79a2d93829
Ian Ibbotson <ianibbo@gmail.com>
2016-04-12 07:08:07 +0100

    Reenabled reconciliation

--- server/gokb/grails-app/controllers/org/gokb/ApiController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/ApiController.groovy
@@ -1025,7 +1025,7 @@ class ApiController {
     "core"                : true,
     "project-mamangement" : true,
     "cell-level-edits"    : true,
-    "es-recon"            : false,
+    "es-recon"            : true,
     "macros"              : true,
   ]
   
