aafa5a2d86de3e003ff9d61ef5ef0bc460c7a164
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-04-18 17:20:43 +0100

    Added script as file

---
+++ server/gokb/grails-app/assets/javascripts/gokb/text-complete.js
@@ -0,0 +1,89 @@
+(function($){
+  
+  /**
+   * Default privately scoped methods and props.
+   */
+  var words = ['apple', 'google', 'facebook', 'github'];
+  function lookup (term, callback) {
+    callback($.map(words, function (word) {
+      
+      if (typeof term !== 'undefined') {
+        return word.indexOf(term) === 0 ? word : null;
+      }
+      
+      return null;
+    }));
+  };
+  
+  var testWords = ['Steve', 'Ian' ,'Gill']; 
+  function testLookup (term, callback) {
+    callback($.map(words, function (word) {
+      
+      if (typeof term !== 'undefined') {
+        return word.indexOf(term) === 0 ? word : null;
+      }
+      
+      return null;
+    }));
+  };
+  
+  /**
+   * Add the text complete functionality.
+   */
+  var addAutoComplete = function ( scope ) {
+    
+    // Default scope to body.
+    if (typeof scope === "undefined") {
+      scope = $("body");
+    }
+    
+    $('.text-complete', $(scope)).each(function(){
+      var me = $(this);
+      var method = me.attr('data-complete-search') || 'lookup';
+      method = eval(method);
+      
+      // If the method was found.
+      if (typeof method === 'function') {
+        // Add the complete.
+        me.textcomplete([{
+          match: /\B@(\w{2,})$/,
+          search: lookup,
+          replace: function (word) {
+            return word + ' ';
+          },
+          index: 1,
+        }]);
+      } else {
+        // Could not find suitable search method.
+        throw "Text Complete: search method not found.";
+      }
+    });
+  };
+//  
+//  // We want to add on default ready event.
+//  $(document).ready(addAutoComplete);
+//  
+//  // We also want to listen for a modal show.
+//  // There is a modal event fired when the "remote" attribute is loaded and shown, but this is now
+//  // deprecated. We will act on show as we do not care how, the modal has been populated. This should
+//  // future proof this code.
+  $(document).on('shown.bs.modal', function (e) {
+    // Us the modal as the context.
+    var modal = $(e.target);
+    addAutoComplete (modal);
+  });
+//  
+////  .textcomplete([
+////    { // GoKBComponent
+////      id: 'gokb-component',
+////      words: ['apple', 'google', 'facebook', 'github'],
+////      match: /\@(\w{2,})$/,
+////      search: lookup,
+////      index: 1,
+////      replace: function (word) {
+////        return word + ' ';
+////      }
+////    }
+////  ] );
+  
+})(jQuery);
