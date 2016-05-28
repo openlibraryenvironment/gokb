af7feadb77db9da3ffc1547c2336d687ff494852
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-05-28 11:35:19 +0100

    Coding elsevier update

--- scripts/sync_elsevier.groovy
+++ scripts/sync_elsevier.groovy
@@ -5,9 +5,11 @@
   @Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
   @Grab(group='javax.mail', module='mail', version='1.4.7'),
   @Grab(group='net.sourceforge.htmlunit', module='htmlunit', version='2.21'),
+  @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2');
+  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.3.5');
+  @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.3.5');
   @GrabExclude('org.codehaus.groovy:groovy-all')
 ])
-
 
 
 import javax.mail.*
@@ -17,8 +19,13 @@ import static groovy.json.JsonOutput.*
 import groovy.json.JsonSlurper
 import java.security.MessageDigest
 import com.gargoylesoftware.htmlunit.*
-
-println "Hello";
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.ContentType.URLENC
+import org.apache.http.entity.mime.MultipartEntity
+import org.apache.http.entity.mime.HttpMultipartMode
+import org.apache.http.entity.mime.content.InputStreamBody
+import org.apache.http.entity.mime.content.StringBody
+import groovyx.net.http.*
 
 config = null;
 cfg_file = new File('./handler-cfg.json')
@@ -43,6 +50,10 @@ cfg_file << toJson(config);
 
 def pullLatest(config, url) {
   def result = false;
+
+  def http = new HTTPBuilder( 'http://localhost:8080' )
+  http.auth.basic 'admin', 'admin'
+
 
   println("Get URL ${url}");
   client = new WebClient()
@@ -71,7 +82,7 @@ def pullLatest(config, url) {
     links.each { link ->
       if ( link.value.startsWith('../holdings/productReport.url') ) {
         def package_name = link.getOwnerElement().getParentNode().getByXPath('../td[@class="report"]/text()');
-        processFile(package_name[0],link.value, config);
+        processFile(package_name[0],link.value, config, http);
         package_count++;
       }
     }
@@ -89,7 +100,7 @@ def pullLatest(config, url) {
   println("Done ${package_count} packages");
 }
 
-def processFile(official_package_name, link, config) {
+def processFile(official_package_name, link, config, http) {
   def url_to_fecth = "http://holdings.sciencedirect.com/"+link.substring(3,link.length())
   println("fetching ${official_package_name} - ${url_to_fecth}");
   def package_data = new URL(url_to_fecth).getText()
@@ -120,27 +131,46 @@ def processFile(official_package_name, link, config) {
   }
   else {
     println("Checksum changed - process file");
-    pushToGokb(official_package_name, package_data);
+    pushToGokb(official_package_name, package_data, http);
     config.packageData[official_package_name].cksum = md5sumHex
     config.packageData[official_package_name].lastProcessed = System.currentTimeMillis()
   }
 
 }
 
-def pushToGokb(name, data) {
+def pushToGokb(name, data, http) {
   // curl -v --user admin:admin -X POST \
-  //   --form content=@./elsevier/ScienceDirectStandard_Global_AllTitles_2016-05-23.txt \
-  //   --form source="ELSEVIER" \
-  //   --form fmt="elsevier" \
-  //   --form pkg="Elsevier Global All Titles" \
-  //   --form platformUrl="http://www.sciencedirect.com/science" \
-  //   --form format="JSON" \
-  //   --form providerName="elsevier" \
-  //   --form providerIdentifierNamespace="ELSEVIER" \
-  //   --form reprocess="Y" \
-  //   --form synchronous="Y" \
-  //   --form flags="+ReviewNewTitles,+ReviewVariantTitles,+ReviewNewOrgs" \
   //   $GOKB_HOST/gokb/packages/deposit
 
+  http.request(Method.POST) { req ->
+    uri.path="/gokb/packages/deposit"
+
+    MultipartEntityBuilder multiPartContent = new MultipartEntityBuilder()
+    // Adding Multi-part file parameter "imageFile"
+    multiPartContent.addPart("content", new InputStreamBody( new ByteArrayInputStream(data.getBytes()), 
+                                        "application/tsv",
+                                        name))
+
+    // Adding another string parameter "city"
+    multiPartContent.addPart("source", new StringBody("ELSEVIER"))
+    multiPartContent.addPart("fmt", new StringBody("elsevier"))
+    multiPartContent.addPart("pkg", new StringBody(name))
+    multiPartContent.addPart("platformUrl", "http://www.sciencedirect.com/science");
+    multiPartContent.addPart("format", "JSON");
+    multiPartContent.addPart("providerName", "elsevier");
+    multiPartContent.addPart("providerIdentifierNamespace", "ELSEVIER");
+    multiPartContent.addPart("reprocess", "Y");
+    multiPartContent.addPart("synchronous", "Y");
+    multiPartContent.addPart("flags", "+ReviewNewTitles,+ReviewVariantTitles,+ReviewNewOrgs");
+    
+    req.entity = multiPartContent.build()
+
+    response.success = { resp, data ->
+      if (resp.statusLine.statusCode == 200) {
+        // response handling
+        println("OK");
+      }
+    }
+  }
 }
 
