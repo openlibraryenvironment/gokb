880d2d0b8e46a46d30f16499015c4311ce33d7f1
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-05-29 11:47:27 +0100

    Added code to override user config in local conf

--- server/gokb/grails-app/conf/BootStrap.groovy
+++ server/gokb/grails-app/conf/BootStrap.groovy
@@ -192,6 +192,9 @@ class BootStrap {
     log.info("Default batch loader config");
     defaultBulkLoaderConfig();
 
+    log.debug("Register users and override default admin password");
+    registerUsers()
+
     log.info("GoKB Initi complete");
   }
 
@@ -885,5 +888,44 @@ class BootStrap {
 
     //log.debug(titleLookupService.getTitleFieldForIdentifier([[ns:'isbn',value:'9780195090017']],'publishedFrom'));
     //log.debug(titleLookupService.getTitleFieldForIdentifier([[ns:'isbn',value:'9780195090017']],'publishedTo'));
+  }
+
+  def registerUsers() {
+
+    grailsApplication.config.sysusers.each { su ->
+      log.debug("test ${su.name} ${su.pass} ${su.display} ${su.roles}");
+      def user = User.findByUsername(su.name)
+      if ( user ) {
+        if ( user.password != su.pass ) {
+          log.debug("Hard change of user password from config ${user.password} -> ${su.pass}");
+          user.password = su.pass;
+          user.save(failOnError: true)
+        }
+        else {
+          log.debug("${su.name} present and correct");
+        }
+      }
+      else {
+        log.debug("Create user...");
+        user = new User(
+                        username: su.name,
+                        password: su.pass,
+                        display: su.display,
+                        email: su.email,
+                        enabled: true).save(failOnError: true)
+      }
+
+      log.debug("Add roles for ${su.name}");
+      su.roles.each { r ->
+        def role = Role.findByAuthority(r)
+        if ( ! ( user.authorities.contains(role) ) ) {
+          log.debug("  -> adding role ${role}");
+          UserRole.create user, role
+        }
+        else {
+          log.debug("  -> ${role} already present");
+        }
+      }
+    }
   }
 }
