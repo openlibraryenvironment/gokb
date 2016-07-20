976c58f9142fdcd02efb09b8b62d02b21342333b
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-07-20 17:25:50 +0100

    updated

--- server/gokb/grails-app/controllers/org/gokb/DecisionSupportController.groovy
+++ server/gokb/grails-app/controllers/org/gokb/DecisionSupportController.groovy
@@ -2,21 +2,25 @@ package org.gokb
 
 import org.springframework.security.access.annotation.Secured;
 import grails.util.GrailsNameUtils
-
 import org.gokb.cred.*
 import org.hibernate.SessionFactory;
 import org.hibernate.transform.AliasToEntityMapResultTransformer
-
 
 class DecisionSupportController {
 
   def grailsApplication
   def springSecurityService
 
-  SessionFactory sessionFactory
-
   @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
   def index() {
+    def result = [:]
+    log.debug("DecisionSupportController::index");
+    result
+  }
+
+
+  def untwo() {
+    log.debug("untwo");
   }
 
 }
