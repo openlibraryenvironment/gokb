73957fbbf01f0a00dd78907ac415c934c6d920ba
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-08-24 13:44:27 +0100

    updated

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,7 +1,10 @@
-docker run -d -p 9201:9201 first-module
+# docker run -d -p 9201:9201 first-module
+docker run -dit gokb
+
+
 
 echo Running docker ps -a
 sudo docker ps -a
 
 echo attach with docker attach ID
-
+echo detach without killing with ctrl-p ctrl-q
