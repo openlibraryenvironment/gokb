0c2984ddbefa221518af86b81dcb17a2e43f91ef
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-09-17 10:04:46 +0100

    Added orgs sync script

--- scripts/full_migration.sh
+++ scripts/full_migration.sh
@@ -1,7 +1,11 @@
+export GOKB_HOST="http://localhost:8080"
+
 
 
 echo 1. Load issn-l data dump
 echo 2. Sync user accounts
+curl -vvv --user admin:admin -X POST --form users=@./users.tsv $GOKB_HOST/gokb/api/bulkLoadUsers
 echo 3. Sync orgs
+
 echo 4. Sync titles
 echo 5. Sync packages
