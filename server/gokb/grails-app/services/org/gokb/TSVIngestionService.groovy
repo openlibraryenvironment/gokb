b98dd01c642d55ecf118f63800c2a66f447f7a5a
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-09-20 10:02:56 +0100

    Extra defensive code

--- server/gokb/grails-app/services/org/gokb/TSVIngestionService.groovy
+++ server/gokb/grails-app/services/org/gokb/TSVIngestionService.groovy
@@ -855,13 +855,18 @@ class TSVIngestionService {
 
 
         Package.withNewTransaction {
-          def update_agent = User.findByUsername('IngestAgent')
-          // insertBenchmark updateBenchmark
-          Package.executeUpdate('update Package p set p.insertBenchmark=:elapsed where p.id=:pid AND p.insertBenchmark is null',
+          try {
+            def update_agent = User.findByUsername('IngestAgent')
+            // insertBenchmark updateBenchmark
+            Package.executeUpdate('update Package p set p.insertBenchmark=:elapsed where p.id=:pid AND p.insertBenchmark is null',
                             [pid:the_package_id,elapsed:processing_elapsed]);
-          Package.executeUpdate('update Package p set p.lastUpdateComment=:uc, p.lastUpdatedBy=:updateAgent, p.updateBenchmark=:elapsed where p.id=:pid',
+
+            Package.executeUpdate('update Package p set p.lastUpdateComment=:uc, p.lastUpdatedBy=:updateAgent, p.updateBenchmark=:elapsed where p.id=:pid',
                             [uc:"Direct ingest of file:${datafile.name}[${datafile.id}] completed in ${processing_elapsed}ms, avg per row=${average_milliseconds_per_row}, avg per hour=${average_per_hour}", pid:the_package_id, updateAgent:update_agent, elapsed:processing_elapsed]);
- 
+          }
+          catch ( Exception e ) {
+            log.warn("Problem updating package stats",e);
+          }
         }
       }
       else {
