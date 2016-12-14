bf73b0b13743a68154b6b39733756d419d5b3df7
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-12-14 17:01:37 +0000

    Added the tags just to the Macro type. They have been removed from the main KBComponent, but the Macro was using them.

--- server/gokb/grails-app/domain/org/gokb/cred/Macro.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/Macro.groovy
@@ -5,9 +5,15 @@ class Macro extends KBComponent {
   String refineTransformations
   String description
   
+  static hasMany = [
+     tags:RefdataValue,
+  ]
+  
   static mapping = {
     includes KBComponent.mapping
     refineTransformations type: 'text'
     description maxSize:128
+    
+    tags joinTable: [name: 'macro_tags_value', key: 'mtgs_kbc_id', column: 'mtgs_rdv_id']
  }
 }
