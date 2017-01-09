6c45843bfef60a62fb538176216bd96f642db5f7
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2017-01-09 14:06:41 +0000

    Changes to SQL storage of JSON and description.

--- server/gokb/grails-app/domain/org/gokb/cred/Macro.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/Macro.groovy
@@ -12,8 +12,12 @@ class Macro extends KBComponent {
   static mapping = {
     includes KBComponent.mapping
     refineTransformations type: 'text'
-    description maxSize:128
+    description type: 'text'
     
     tags joinTable: [name: 'macro_tags_value', key: 'mtgs_kbc_id', column: 'mtgs_rdv_id']
  }
+  static constraints = {
+    description nullable:true, blank:false
+    refineTransformations nullable:true, blank:false
+  }
 }
