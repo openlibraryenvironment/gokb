f2d2eb079c44525aae7e18f156091f7f661f2925
Ian Ibbotson <ianibbo@gmail.com>
2018-01-26 16:35:33 +0000

    Jiggle copy_to for es5 semantics

--- scripts/es_reset.sh
+++ scripts/es_reset.sh
@@ -31,9 +31,10 @@ curl -X PUT "localhost:9200/gokb/component/_mapping" -d '{
   "component" : {
     "properties" : {
       "name" : {
-        "type" : "multi_field",
+        "type" : "text",
+	"copy_to" : "suggest",
         "fields" : {
-          "name" : { "type" : "string", "analyzer" : "snowball", "copy_to" : "suggest" },
+          "name" : { "type" : "string", "analyzer" : "snowball" },
           "altname" : { "type" : "string", "analyzer" : "snowball" }
         }
       },
