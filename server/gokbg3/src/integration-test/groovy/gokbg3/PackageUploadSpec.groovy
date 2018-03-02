a81440801c43c67e1305a13e470ca3f70511e0b8
Ian Ibbotson <ianibbo@gmail.com>
2018-03-02 23:47:58 +0000

    Moved old integration tests to attic dir, example integration test set up

---
+++ server/gokbg3/src/integration-test/groovy/gokbg3/PackageUploadSpec.groovy
@@ -0,0 +1,33 @@
+package gokbg3
+
+import grails.testing.mixin.integration.Integration
+import grails.transaction.*
+import spock.lang.Specification
+import spock.lang.Shared
+import grails.plugins.rest.client.RestBuilder
+import grails.plugins.rest.client.RestResponse
+
+
+@Integration
+@Rollback
+class PackageUploadSpec extends Specification {
+
+    @Shared
+    RestBuilder rest = new RestBuilder()
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test search"() {
+      when:
+        // RestResponse resp = rest.get("http://localhost:${serverPort}/search/search")
+        RestResponse resp = rest.get("http://localhost:${serverPort}/")
+
+      then:
+        // println(resp.json)
+        resp.status == 200
+    }
+}
