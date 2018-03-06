75a6b2a3e318e3bde2e71dec46cc74073fd1cdf6
Ian Ibbotson <ian.ibbotson@k-int.com>
2018-03-06 17:51:18 +0000

    Adding  testRuntime 'com.bertramlabs.plugins:less-asset-pipeline:2.14.8' means that functional tests get proper stylesheets so report(...) screenshots look right

--- server/gokbg3/grails-app/conf/runtime.groovy
+++ server/gokbg3/grails-app/conf/runtime.groovy
@@ -558,7 +558,7 @@ apiClasses = [
 ]
 
 /** Less config **/
-
+/** Config duplicated here and in build.groovy for alternate run paths */
 grails.assets.less.compiler = 'less4j'
 grails.assets.excludes = [ '**/_*.less', 'gokb/themes/*.less', 'gokb/themes/**/*.less', 'bootstrap/*.less', 'bootstrap/**/*.less' ]
 grails.assets.includes = [ 'webfonts/*', 'gokb/themes/*/theme.less', 'bootstrap/bootstrap.less' ]
