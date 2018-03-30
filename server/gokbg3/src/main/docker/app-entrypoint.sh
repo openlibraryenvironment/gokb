6b024a35cd111072a89267ae256be7b27f048e22
Ian Ibbotson <ian.ibbotson@k-int.com>
2018-03-30 11:21:27 +0100

    Allow docker environment settings to override config to set es cluster name and db params

--- server/gokbg3/src/main/docker/app-entrypoint.sh
+++ server/gokbg3/src/main/docker/app-entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -Djava.security.egd=file:/dev/./urandom -jar /app/application.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /app/application.jar --gokb.es.cluster=$GOKB_ES_CLUSTER --dataSource.url=${GOKB_DB} --dataSource.username=${GOKB_DB_USER} --dataSource.password=${GOKB_DB_PASS}
+# java -Djava.security.egd=file:/dev/./urandom -jar /app/application.jar --gokb.es.cluster=$GOKB_ES_CLUSTER --dataSource.url=${GOKB_DB} --dataSource.username=${GOKB_DB_USER} --dataSource.password=${GOKB_DB_PASS}
