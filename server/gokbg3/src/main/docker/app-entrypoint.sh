6ce9dc6556eb9c6bcb1cf99069d3e95c73e64a83
Ian Ibbotson <ianibbo@gmail.com>
2018-04-03 12:37:08 +0100

    Pass docker environment for GOKB_DEFAULT_CG to command line

--- server/gokbg3/src/main/docker/app-entrypoint.sh
+++ server/gokbg3/src/main/docker/app-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-java -Djava.security.egd=file:/dev/./urandom -jar /app/application.jar --gokb.es.cluster=$GOKB_ES_CLUSTER --dataSource.url=${GOKB_DB} --dataSource.username=${GOKB_DB_USER} --dataSource.password=${GOKB_DB_PASS}
-# java -Djava.security.egd=file:/dev/./urandom -jar /app/application.jar --gokb.es.cluster=$GOKB_ES_CLUSTER --dataSource.url=${GOKB_DB} --dataSource.username=${GOKB_DB_USER} --dataSource.password=${GOKB_DB_PASS}
+java -Djava.security.egd=file:/dev/./urandom -jar /app/application.jar \
+	--gokb.es.cluster=$GOKB_ES_CLUSTER \
+	--gokb.defaultCuratoryGroup=${GOKB_DEFAULT_CG} \
+	--dataSource.url=${GOKB_DB} \
+	--dataSource.username=${GOKB_DB_USER} \
+	--dataSource.password=${GOKB_DB_PASS}
