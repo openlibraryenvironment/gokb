4bc21d6829ba5c91732fdd3258b31d6b82dc5491
Ian Ibbotson <ianibbo@gmail.com>
2018-05-11 19:59:24 +0100

    Added content item as a way to do customisation of message resources

---
+++ server/gokbg3/grails-app/domain/org/gokb/cred/ContentItem.groovy
@@ -0,0 +1,31 @@
+package org.gokb.cred;
+
+class ContentItem {
+
+  String key
+  String locale
+  String content
+
+  static mapping = {
+         id column:'ci_id'
+        key column:'ci_key'
+     locale column:'ci_locale'
+    content column:'ci_content', type:'text'
+  }
+
+  static constraints = {
+        key(nullable:false, blank:false)
+     locale(nullable:false, blank:true)
+    content(nullable:false, blank:false)
+  }
+
+  static def lookupOrCreate(key,locale,content) {
+    def result = ContentItem.findByKeyAndLocale(key,locale)
+    if ( result == null ) {
+      result = new ContentItem(key:key, locale:locale, content:content);
+      result.locale = locale
+      result.save()
+    }
+    result
+  }
+}
