564bc1d01fc3d9293d4b7253bc8c80151e4d3483
volker <volker.padur@hbz-nrw.de>
2020-01-09 11:47:01 +0100

    additional array with roles in the profile returned by show()

--- server/gokbg3/grails-app/controllers/org/gokb/rest/ProfileController.groovy
+++ server/gokbg3/grails-app/controllers/org/gokb/rest/ProfileController.groovy
@@ -2,11 +2,13 @@ package org.gokb.rest
 
 import grails.converters.*
 import grails.gorm.transactions.*
+import org.gokb.cred.Role
+import org.gokb.cred.User
+
 import static org.springframework.http.HttpStatus.*
 import static org.springframework.http.HttpMethod.*
 import org.springframework.security.acls.model.NotFoundException
 import org.springframework.security.access.annotation.Secured;
-import org.gokb.cred.*
 import java.security.MessageDigest
 import grails.converters.JSON
 
@@ -32,17 +34,24 @@ class ProfileController {
       cur_groups.add([name: cg.name, id: cg.id, uuid: cg.uuid])
     }
 
+    def roles = []
+    Role.findAll().each { role ->
+      if (user.hasRole(role.authority))
+        roles.add(role)
+    }
+
     result = [
-      'id': user.id,
-      'username': user.username,
-      'displayName': user.displayName,
-      'email': user.email,
-      'curatoryGroups': cur_groups,
-      'enabled' : user.enabled,
-      'accountExpired' : user.accountExpired,
-      'accountLocked' : user.accountLocked,
-      'passwordExpired' : user.accountExpired,
-      'defaultPageSize' : user.defaultPageSize
+        'id'             : user.id,
+        'username'       : user.username,
+        'displayName'    : user.displayName,
+        'email'          : user.email,
+        'curatoryGroups' : cur_groups,
+        'enabled'        : user.enabled,
+        'accountExpired' : user.accountExpired,
+        'accountLocked'  : user.accountLocked,
+        'passwordExpired': user.accountExpired,
+        'defaultPageSize': user.defaultPageSize,
+        'roles'          : roles
     ]
 
     render result as JSON
@@ -50,8 +59,8 @@ class ProfileController {
 
   @Transactional
   def update() {
-    def result = ['result':'OK']
-    def immutables = ['id','username','enabled','accountExpired','accountLocked','passwordExpired','last_alert_check']
+    def result = ['result': 'OK']
+    def immutables = ['id', 'username', 'enabled', 'accountExpired', 'accountLocked', 'passwordExpired', 'last_alert_check']
     def errors = []
     def skippedCG = false
     def reqBody = request.JSON
@@ -60,69 +69,62 @@ class ProfileController {
 
     if (reqBody && reqBody.id && reqBody.id == user.id) {
       reqBody.each { field, val ->
-        if ( val && user.hasProperty(field) ) {
-          if ( field != 'curatoryGroups' ) {
-            if ( !immutables.contains(field) ) {
+        if (val && user.hasProperty(field)) {
+          if (field != 'curatoryGroups') {
+            if (!immutables.contains(field)) {
               user."${field}" = val
-            }
-            else {
+            } else {
               log.debug("Ignoring immutable field ${field}")
             }
-          }
-          else {
-            if ( user.hasRole('ROLE_EDITOR') || user.hasRole('ROLE_ADMIN') ) {
+          } else {
+            if (user.hasRole('ROLE_EDITOR') || user.hasRole('ROLE_ADMIN')) {
               def curGroups = []
-              val.each { cg -> 
+              val.each { cg ->
                 def cg_obj = null
 
-                if( cg.uuid?.trim() ) {
+                if (cg.uuid?.trim()) {
                   cg_obj = CuratoryGroup.findByUuid(cg.uuid)
                 }
-                
-                if( !cg_obj && cg.id ) {
+
+                if (!cg_obj && cg.id) {
                   cg_obj = cg.id instanceof String ? genericOIDService.resolveOID(cg.id) : CuratoryGroup.get(cg.id)
                 }
 
-                if ( cg_obj ) {
+                if (cg_obj) {
                   curGroups.add(cg_obj)
-                }
-                else {
+                } else {
                   log.debug("CuratoryGroup ${cg} not found!")
                   errors << ['message': 'Could not find referenced curatory group!', 'baddata': cg]
                 }
               }
               log.debug("New CuratoryGroups: ${curGroups}")
 
-              if ( errors.size() > 0 ) {
+              if (errors.size() > 0) {
                 result.message = "There have been errors updating the users curatory groups."
                 result.errors = errors
                 response.setStatus(400)
-              }
-              else {
+              } else {
                 user.curatoryGroups.addAll(curGroups)
                 user.curatoryGroups.retainAll(curGroups)
               }
-            }
-            else {
+            } else {
               skippedCG = true
             }
           }
         }
       }
 
-      if( errors.size() == 0 ) {
-        if ( user.validate() ) {
-          user.save(flush:true)
+      if (errors.size() == 0) {
+        if (user.validate()) {
+          user.save(flush: true)
           result.message = "User profile sucessfully updated."
-        }
-        else {
+        } else {
           result.result = "ERROR"
           result.message = "There have been errors saving the user object."
           result.errors = user.errors
         }
       }
-    }
-    else {
+    } else {
       log.debug("Missing update payload or wrong user id")
       result.result = "ERROR"
       response.setStatus(400)
