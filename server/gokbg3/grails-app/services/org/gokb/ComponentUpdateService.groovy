afd96f3f4c2d8ed5e079038442af1ada30cb2506
hornmo <moritz.horn@gmail.com>
2020-05-14 18:41:20 +0200

    update oai layout

--- server/gokbg3/grails-app/services/org/gokb/ComponentUpdateService.groovy
+++ server/gokbg3/grails-app/services/org/gokb/ComponentUpdateService.groovy
@@ -162,24 +162,30 @@ class ComponentUpdateService {
 
     // If this is a component that supports curatoryGroups we should check for them.
     if ( KBComponent.has(component, 'curatoryGroups') ) {
+      log.debug("Handling Curatory Groups ..")
       def groups = component.curatoryGroups.collect { [id: it.id, name: it.name] }
 
       data.curatoryGroups?.each { String name ->
         if (!groups.find {it.name.toLowerCase() == name.toLowerCase()}) {
 
           def group = CuratoryGroup.findByNormname(CuratoryGroup.generateNormname(name))
+          def combo_type_cg = RefdataCategory.lookup('Combo.Type', component.getComboTypeValue('curatoryGroups'))
           // Only add if we have the group already in the system.
           if (group) {
-            component.addToCuratoryGroups ( group )
+            log.debug("Adding group ${name}..")
+            def new_combo = new Combo(fromComponent:component, toComponent:group, type:combo_type_cg, status:combo_active).save(flush:true, failOnError:true)
             hasChanged = true
-            groups << [id: it.id, name: it.name]
+            groups << [id: group.id, name: group.name]
+          }
+          else {
+            log.debug("Could not find linked group ${name}!")
           }
         }
       }
 
       if (sync) {
         groups.each { cg ->
-          if (!data.curatoryGroups || !data.curatoryGroups.contains(cg.name)) {
+          if (!data.curatoryGroups || !data.curatoryGroups.find {it.toLowerCase() == cg.name.toLowerCase()}) {
             log.debug("Removing deprecated CG ${cg.name}")
             Combo.executeUpdate("delete from Combo as c where c.fromComponent = ? and c.toComponent.id = ?", [component, cg.id])
             component.refresh()
@@ -212,7 +218,6 @@ class ComponentUpdateService {
             prop.propertyDefn = pType
             prop.apValue = it.value
             component.addToAdditionalProperties(prop)
-            component.save(failOnError: true)
             props << testKey
           }
         }
@@ -251,7 +256,7 @@ class ComponentUpdateService {
     if (hasChanged) {
       component.lastSeen = new Date().getTime()
     }
-    component.save()
+    component.merge(flush:true)
 
     hasChanged
   }
@@ -262,5 +267,49 @@ class ComponentUpdateService {
       changed |= ClassUtils.setRefdataIfPresent(data[prop], target, prop, createNew)
     }
     changed
+  }
+
+  private def createOrUpdateSource( data ) {
+    log.debug("assertSource, data = ${data}");
+    def result=[:]
+    def source_data = data;
+    def changed = false
+    result.status = true;
+
+    try {
+      if ( data.name ) {
+
+        Source.withNewSession {
+          def located_or_new_source = Source.findByNormname( Source.generateNormname(data.name) ) ?: new Source(name:data.name).save(flush:true, failOnError:true)
+
+          ClassUtils.setStringIfDifferent(located_or_new_source,'url',data.url)
+          ClassUtils.setStringIfDifferent(located_or_new_source,'defaultAccessURL',data.defaultAccessURL)
+          ClassUtils.setStringIfDifferent(located_or_new_source,'explanationAtSource',data.explanationAtSource)
+          ClassUtils.setStringIfDifferent(located_or_new_source,'contextualNotes',data.contextualNotes)
+          ClassUtils.setStringIfDifferent(located_or_new_source,'frequency',data.frequency)
+          ClassUtils.setStringIfDifferent(located_or_new_source,'ruleset',data.ruleset)
+
+          changed |= setAllRefdata ([
+            'software', 'service'
+          ], source_data, located_or_new_source)
+
+          ClassUtils.setRefdataIfPresent(data.defaultSupplyMethod, located_or_new_source, 'defaultSupplyMethod', 'Source.DataSupplyMethod')
+          ClassUtils.setRefdataIfPresent(data.defaultDataFormat, located_or_new_source, 'defaultDataFormat', 'Source.DataFormat')
+
+          log.debug("Variant names processing: ${data.variantNames}")
+
+          // variants
+          data.variantNames.each { vn ->
+            addVariantNameToComponent(located_or_new_source, vn)
+          }
+
+          result['component'] = located_or_new_source
+        }
+      }
+    }
+    catch ( Exception e ) {
+      e.printStackTrace()
+    }
+    result
   }
 }
