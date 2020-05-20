0a317baf3d1fa8f4a8ca2166c69650ca83733c1b
VolkerHBZ <volker.padur@hbz-nrw.de>
2020-05-20 17:44:10 +0200

    making tests work... kinda

--- server/gokbg3/src/integration-test/groovy/gokbg3/rest/ReviewsTestSpec.groovy
+++ server/gokbg3/src/integration-test/groovy/gokbg3/rest/ReviewsTestSpec.groovy
@@ -1,13 +1,31 @@
 package gokbg3.rest
 
+import grails.gorm.transactions.Transactional
 import grails.plugins.rest.client.RestBuilder
 import grails.plugins.rest.client.RestResponse
 import grails.testing.mixin.integration.Integration
+import org.gokb.cred.ReviewRequest
+import org.gokb.cred.TitleInstance
 
 @Integration
 class ReviewsTestSpec extends AbstractAuthSpec {
 
   private RestBuilder rest = new RestBuilder()
+  private ReviewRequest rr
+  private TitleInstance title
+
+  @Transactional
+  def setup() {
+    title = TitleInstance.findOrCreateWhere(name: "testTitle for integration testing").save(flush: true, failOnError: true)
+    rr = ReviewRequest.findOrCreateWhere(reviewRequest: "fake review request for integration testing", componentToReview:title)
+    rr.save(flush: true, failOnError: true)
+  }
+
+  @Transactional
+  def cleanup() {
+    rr.delete(flush: true)
+    title.delete(flush: true)
+  }
 
   void "test GET /rest/reviews with params"() {
     // use the bearerToken to write to /authRest/user
@@ -21,5 +39,6 @@ class ReviewsTestSpec extends AbstractAuthSpec {
     }
     then:
     resp.status == 200
+    resp.json.data.size() == 1
   }
 }
