292623ae26ab5d19d4bf5ff9f933ac3edda2f589
VolkerHBZ <volker.padur@hbz-nrw.de>
2020-10-27 09:58:37 +0100

    fix tests

--- server/gokbg3/src/integration-test/groovy/gokbg3/rest/SourcesTestSpec.groovy
+++ server/gokbg3/src/integration-test/groovy/gokbg3/rest/SourcesTestSpec.groovy
@@ -36,7 +36,7 @@ class SourcesTestSpec extends AbstractAuthSpec {
     }
     then:
     resp.status == 200
-    resp.json.data.size() == 8
+    resp.json.data.size() == 7
   }
 
   void "test GET /rest/sources/{id}"() {
