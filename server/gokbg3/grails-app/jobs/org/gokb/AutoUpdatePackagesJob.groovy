a7747faa3b30c9646d82e1ae699568b2ee7c2598
VolkerHBZ <volker.padur@hbz-nrw.de>
2020-11-12 09:24:53 +0100

    added configuration options to application.yml checking the update frequency against lastRun before sending the update request to Ygor the frequency string consists of a number followed by a letter with the letter representing the units (Day, Week, Month, Quarter, Half, Year) like 1W or 5M. whitespaces are ignored, the number and letter are both mandatory.

--- server/gokbg3/grails-app/jobs/org/gokb/AutoUpdatePackagesJob.groovy
+++ server/gokbg3/grails-app/jobs/org/gokb/AutoUpdatePackagesJob.groovy
@@ -2,6 +2,10 @@ package org.gokb
 
 import groovyx.net.http.RESTClient
 import org.gokb.cred.Package
+import org.gokb.cred.Source
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 import static groovyx.net.http.Method.GET
 
@@ -18,7 +22,7 @@ class AutoUpdatePackagesJob {
   def execute() {
     if (!concurrent) {
       concurrent = true;
-      if (grailsApplication.config.gokb.packageUpdate_enabled && grailsApplication.config.gokb.ygorUrl) {
+      if (grailsApplication.config.gokb.packageUpdate.enabled && grailsApplication.config.gokb.ygorUrl) {
         log.debug("Beginning scheduled auto update packages job.")
         def endpoint = grailsApplication.config.gokb.ygorUrl
         def target_service
@@ -26,26 +30,28 @@ class AutoUpdatePackagesJob {
         // find all updateable packages
         def updPacks = Package.executeQuery("from Package p where p.source != null and p.source.automaticUpdates = true and (p.source.lastRun = null or p.source.lastRun < current_date)")
         updPacks.each { Package p ->
-          def error = false
-          def path = "/enrichment/processGokbPackage?pkgId=${p.id}&updateToken=${p.updateToken}"
-          target_service = new RESTClient(endpoint + path)
-          try {
-            target_service.request(GET) { request ->
-              response.success = { resp, data ->
-                respData = data
-              }
-              response.failure = { resp ->
-                log.error("Error - ${resp}");
-                error = true
+          if (needsUpdate(p)) {
+            def error = false
+            def path = "/enrichment/processGokbPackage?pkgId=${p.id}&updateToken=${p.updateToken}"
+            target_service = new RESTClient(endpoint + path)
+            try {
+              target_service.request(GET) { request ->
+                response.success = { resp, data ->
+                  respData = data
+                }
+                response.failure = { resp ->
+                  log.error("Error - ${resp}");
+                  error = true
+                }
               }
             }
-          }
-          catch (Exception e) {
-            e.printStackTrace();
-          }
-          // TODO: asynchronous calls to avoid timeouts followed by waiting for status=finished before next package
-          if (!error) {
-            p.source.lastRun = new Date()
+            catch (Exception e) {
+              e.printStackTrace();
+            }
+            // TODO: asynchronous calls to avoid timeouts followed by waiting for status=finished before next package
+            if (!error) {
+              p.source.lastRun = new Date()
+            }
           }
         }
         log.info("auto update packages job completed.")
@@ -54,5 +60,36 @@ class AutoUpdatePackagesJob {
       }
     }
     concurrent = false
+  }
+
+  private boolean needsUpdate(Package pack) {
+    Source src = pack.source
+    if (src.lastRun == null) {
+      return true;
+    }
+    if (src.frequency != null) {
+      Pattern pattern = Pattern.compile("\\s*(\\d*)\\s*([a-zA-Z]*)\\s*");
+      Matcher matcher = pattern.matcher(src.frequency);
+      int days
+      if (matcher.find()) {
+        def length = [
+          D: 1,
+          T: 1,
+          W: 7,
+          M: 30,
+          Q: 91,
+          H: 182,
+          J: 365,
+          Y: 365]
+        def interval = matcher.group(2)
+        def number = matcher.group(1)
+        days = Integer.parseInt(number) * length[interval.toUpperCase()]
+      }
+      Date today = new Date()
+      if (src.lastRun.plus(days) < today) {
+        return true
+      }
+    }
+    return false
   }
 }
