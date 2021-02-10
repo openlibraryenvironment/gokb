41e2033e5ba552e24a1779b3fb8db2d1ef11cda5
Philipp von Böselager <philipp.boeselager@web.de>
2021-02-10 14:34:28 +0100

    Unify Elasticsearch settings of es_reset.sh and ESWrapperService

--- scripts/es_reset.sh
+++ scripts/es_reset.sh
@@ -1,6 +1,7 @@
 
 export INDEXNAME="${1:-gokb}"
 export MAPPING_FILE="src/elasticsearch/es_mapping.json"
+export SETTINGS_FILE="src/elasticsearch/es_settings.json"
 
 echo Reset ES indexes for index name \"$INDEXNAME\"
 
@@ -8,30 +9,7 @@ echo Drop old index
 curl -XDELETE "http://localhost:9200/$INDEXNAME"
 
 echo \\nCreate index
-curl -X PUT "localhost:9200/$INDEXNAME" -H 'Content-Type: application/json' -d '{
-  "settings": {
-      "number_of_shards": 1,
-      "analysis": {
-          "filter": {
-              "autocomplete_filter": {
-                  "type":     "edge_ngram",
-                  "min_gram": 1,
-                  "max_gram": 20
-              }
-          },
-          "analyzer": {
-              "autocomplete": {
-                  "type":      "custom",
-                  "tokenizer": "standard",
-                  "filter": [
-                      "lowercase",
-                      "autocomplete_filter"
-                  ]
-              }
-          }
-      }
-  }
-}'
+curl -X PUT "localhost:9200/$INDEXNAME" -H 'Content-Type: application/json' -d@$SETTINGS_FILE
 
 echo \\nCreate component mapping
 curl -X PUT "localhost:9200/$INDEXNAME/component/_mapping" -H "Content-Type: application/json" -d@$MAPPING_FILE
--- server/gokbg3/grails-app/services/org/gokb/ESWrapperService.groovy
+++ server/gokbg3/grails-app/services/org/gokb/ESWrapperService.groovy
@@ -15,6 +15,7 @@ class ESWrapperService {
 
   TransportClient esclient = null
   File mappingFile = new File("src${File.separator}elasticsearch${File.separator}es_mapping.json")
+  File settingsFile = new File("src${File.separator}elasticsearch${File.separator}es_settings.json")
 
   @javax.annotation.PostConstruct
   def init() {
@@ -22,34 +23,12 @@ class ESWrapperService {
   }
 
   def getSettings() {
-    def settings = [
-      number_of_shards: 1,
-      analysis: [
-        filter: [
-          autocomplete_filter: [
-            type: "edge_ngram",
-            min_gram: 1,
-            max_gram: 20
-          ]
-        ],
-        analyzer: [
-          autocomplete: [
-            type: "custom",
-            tokenizer: "standard",
-            filter: ["lowercase","autocomplete_filter"]
-          ]
-        ]
-      ]
-    ]
+    def settings = new JsonSlurper().parse(settingsFile)
     return settings
   }
 
   def getMapping() {
-    def mapping = new JsonSlurper().parse(new File(mappingFile))
-
-
-
-
+    def mapping = new JsonSlurper().parse(mappingFile)
     return mapping
   }
 
