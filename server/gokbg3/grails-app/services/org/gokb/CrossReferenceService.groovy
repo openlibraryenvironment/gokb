c1847695f51aa0c6f0003c938b2ce2a1120d4039
VolkerHBZ <volker.padur@hbz-nrw.de>
2021-06-25 12:58:35 +0200

    revert session handling

--- server/gokbg3/grails-app/services/org/gokb/CrossReferenceService.groovy
+++ server/gokbg3/grails-app/services/org/gokb/CrossReferenceService.groovy
@@ -3,15 +3,10 @@ package org.gokb
 import com.k_int.ConcurrencyManagerService.Job
 import groovy.util.logging.*
 import org.gokb.cred.Package
-import org.hibernate.SessionFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Async
 
 @Slf4j
 class CrossReferenceService {
-
-  @Autowired
-  SessionFactory sessionFactory
 
   def xRefPkg(def rjson, boolean addOnly, boolean fullsync, def auto, def request_locale, def request_user, Job job = null) {
     Package.withSession {
@@ -22,7 +17,13 @@ class CrossReferenceService {
 
   @Async
   def updatePackage(def rjson, boolean addOnly, boolean fullsync, def auto, def request_locale, def request_user, Job job = null) {
-    sessionFactory.currentSession.with {
+    if (!job) {
+      Package.withSession {
+        UpdatePkgTippsRun myRun = new UpdatePkgTippsRun(rjson, addOnly, fullsync, auto, request_locale, request_user)
+        return myRun.work(job)
+      }
+    }
+    Package.withNewSession {
       UpdatePkgTippsRun myRun = new UpdatePkgTippsRun(rjson, addOnly, fullsync, auto, request_locale, request_user)
       return myRun.work(job)
     }
