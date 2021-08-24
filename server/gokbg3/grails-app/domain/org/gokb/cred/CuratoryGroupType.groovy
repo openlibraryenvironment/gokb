3284f036b3d8ed620a9c501dda016e62c8bdae24
Philipp von Böselager <philipp.boeselager@web.de>
2021-08-24 15:49:30 +0200

    Reformat CuratoryGroupType (GOKbIT-83)

--- server/gokbg3/grails-app/domain/org/gokb/cred/CuratoryGroupType.groovy
+++ server/gokbg3/grails-app/domain/org/gokb/cred/CuratoryGroupType.groovy
@@ -2,37 +2,37 @@ package org.gokb.cred
 
 class CuratoryGroupType extends KBComponent{
 
-    // TODO: frontend admin section
+  // TODO: frontend admin section
 
-    Level level
-    String name
+  Level level
+  String name
 
-    static constraints = {
-        level (nullable:false, blank:false)
-        name (nullable:false, blank:false)
+  static constraints = {
+    level (nullable:false, blank:false)
+    name (nullable:false, blank:false)
+  }
+
+
+  static enum Level{
+    PACKAGE  ("PACKAGE", 25),
+    TITLE    ("TITLE", 50),
+    CENTRAL  ("CENTRAL", 75)
+
+    private final String name
+    private final int levelNumber
+
+
+    private Level(String name) {
+      this.name = name
     }
 
-
-    static enum Level{
-        PACKAGE    ("PACKAGE", 25),
-        TITLE      ("TITLE", 50),
-        CENTRAL    ("CENTRAL", 75)
-
-        private final String name
-        private final int levelNumber
-
-
-        private Level(String name) {
-            this.name = name
+    Level getByName(String name){
+      for (Level level : Level.values()) {
+        if (level.name().equalsIgnoreCase(name)) {
+          return level
         }
-
-        Level getByName(String name){
-            for (Level level : Level.values()) {
-                if (level.name().equalsIgnoreCase(name)) {
-                    return level
-                }
-            }
-            return null
-        }
+      }
+      return null
     }
+  }
 }
