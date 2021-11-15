dff61174000d4a789d01f7967bbdaf9f7de02831
Philipp von Böselager <philipp.boeselager@web.de>
2021-11-15 16:00:26 +0100

    Fix es config files paths in es_reset.sh (GOKbIT-191)

--- scripts/es_reset.sh
+++ scripts/es_reset.sh
@@ -1,8 +1,8 @@
 
 export INDEXNAME="${1:-gokb}"
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export MAPPING_FILE=$SCRIPT_DIR"/../server/gokbg3/src/elasticsearch/es_mapping.json"
-export SETTINGS_FILE=$SCRIPT_DIR"/../server/gokbg3/src/elasticsearch/es_settings.json"
+export MAPPING_FILE=$SCRIPT_DIR"/../server/gokbg3/grails-app/conf/elasticsearch/es_mapping.json"
+export SETTINGS_FILE=$SCRIPT_DIR"/../server/gokbg3/grails-app/conf/elasticsearch/es_settings.json"
 
 printf 'Reset ES indexes for index name \"$INDEXNAME\"\n'
 
