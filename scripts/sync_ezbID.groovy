3fe1043f59bee478c04ca102302a678b9d385ebd
Philipp v. Böselager <philipp.boeselager@web.de>
2022-05-03 19:23:40 +0200

    Develop first-time-EZB-enrichment script (context of GOKbIT-139)

--- scripts/sync_ezbID.groovy
+++ scripts/sync_ezbID.groovy
@@ -1,44 +1,23 @@
 #!groovy
 
 @Grapes([
-  @GrabResolver(name='mvnRepository', root='http://central.maven.org/maven2/'),
-  @Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
-  @Grab(group='javax.mail', module='mail', version='1.4.7'),
-  @Grab(group='net.sourceforge.htmlunit', module='htmlunit', version='2.21'),
-  @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2'),
-  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.5.2'),
-  @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.5.2'),
-  @Grab(group='commons-net', module='commons-net', version='3.5'),
-  @Grab(group='net.sf.opencsv', module='opencsv', version='2.0'),
-  @Grab(group='commons-io', module='commons-io', version='2.5'),
-  @GrabExclude('org.codehaus.groovy:groovy-all')
+    @GrabResolver(name='mvnRepository', root='http://central.maven.org/maven2/'),
+    @Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
+    @Grab(group='javax.mail', module='mail', version='1.4.7'),
+    @Grab(group='net.sourceforge.htmlunit', module='htmlunit', version='2.21'),
+    @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2'),
+    @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.5.2'),
+    @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.5.2'),
+    @Grab(group='commons-net', module='commons-net', version='3.5'),
+    @Grab(group='net.sf.opencsv', module='opencsv', version='2.0'),
+    @Grab(group='commons-io', module='commons-io', version='2.5'),
+    @GrabExclude('org.codehaus.groovy:groovy-all')
 ])
-
-
-import static groovyx.net.http.ContentType.XML
-import static groovyx.net.http.Method.GET
-import static groovy.json.JsonOutput.*
 
 import groovy.json.JsonSlurper
 import groovyx.net.http.*
-import java.util.Properties
-import java.security.MessageDigest
-import javax.mail.*
-import javax.mail.search.*
-
-import org.apache.commons.io.ByteOrderMark
-import org.apache.commons.io.IOUtils
-import org.apache.commons.net.ftp.*
-import org.apache.http.entity.mime.HttpMultipartMode
-import org.apache.http.entity.mime.MultipartEntity
-import org.apache.http.entity.mime.MultipartEntityBuilder
-import org.apache.http.entity.mime.content.ByteArrayBody
-import org.apache.http.entity.mime.content.InputStreamBody
-import org.apache.http.entity.mime.content.StringBody
-
-import com.gargoylesoftware.htmlunit.*
-import au.com.bytecode.opencsv.CSVReader
-
+import org.apache.commons.io.*
+import au.com.bytecode.opencsv.*
 
 def config = null
 
@@ -57,6 +36,7 @@ config.uploadPass = System.console().readPassword ('Enter your password: ').toSt
 // config.uploadUser = "You can also set an automated username here."
 // config.uploadPass = "You can also set an automated password here."
 
+
 def httpbuilder = new HTTPBuilder( "http://localhost:${config.port}" )
 httpbuilder.auth.basic config.uploadUser, config.uploadPass
 
@@ -66,21 +46,21 @@ println("Enriching EZB IDs by CSV file ... done.")
 
 
 
-
 def enrichEzb(config, httpbuilder, issnfile) {
   File f = new File(args[0])
   def charset = 'UTF-8'
   def csv = new CSVReader(new InputStreamReader(
-                            new org.apache.commons.io.input.BOMInputStream(
-                              new FileInputStream(issnfile),
-                                ByteOrderMark.UTF_16LE, ByteOrderMark.UTF_16BE, ByteOrderMark.UTF_32LE, ByteOrderMark.UTF_32BE,ByteOrderMark.UTF_8),
-                            java.nio.charset.Charset.forName(charset)),'\t' as char,'\0' as char)
+      new org.apache.commons.io.input.BOMInputStream(
+          new FileInputStream(issnfile),
+          ByteOrderMark.UTF_16LE, ByteOrderMark.UTF_16BE, ByteOrderMark.UTF_32LE, ByteOrderMark.UTF_32BE,ByteOrderMark.UTF_8),
+      java.nio.charset.Charset.forName(charset)),'\t' as char,'\0' as char)
 
   String[] header = csv.readNext()
-  println(header)
+  println "Found header: ${header}"
   String[] nl=csv.readNext()
 
   int rownum = 1
+  int enrichedRecords = 0
 
   while (nl != null) {
     if (rownum % 5000 == 0) {
@@ -90,92 +70,112 @@ def enrichEzb(config, httpbuilder, issnfile) {
       }
     }
 
-    // println("${rownum} ${nl}")
+    println("Row: ${rownum}")
 
-    def identifiers = []
     def title
     def zdbId
+    def edbId
 
     def cell = 0
     nl.each {
       if ( cell == 0 ) {
-		zdbId = it
-        identifiers.add( [ type:'zdb', value:it ] )
+        zdbId = it
       }
       else if ( cell == 1 ) {
-        identifiers.add( [ type:'ezb', value:it ] )
+        ezbId = it
       }
       else if ( cell == 2 ) {
-		title = it
+        title = it
       }
       cell++
     }
+
+    if (String.valueOf(zdbId).length() == 7){
+      zdbId = String.valueOf(zdbId).concat("-").concat(getZdbCheckDigit(zdbId))
+    }
+
+    def records = getRecords(config, zdbId)
+
+    println("Row number: ${rownum}")
+    println("Got records: ${records}")
+    println("Enriched records: ${enrichedRecords}")
+
+    for (def rec in records){
+      def ids = rec.identifiers
+      ids.add( [ type:'ezb', value:${ezbId} ] )
+      rec.identifiers = ids
+      addToGoKB(httpbuilder, rec, config)
+      enrichedRecords++
+    }
+
+    if (enrichedRecords >= 1){
+      break
+    }
     rownum++
-
-    def gokbEntry = queryZdbId(httpbuilder, config, zdbId)
-
-    println "Get response: ${gokbEntry}"
-
-/*
- *  TODO
- *  addToGoKB(false, httpbuilder, 'Unknown Title '+ nl[0],'Serial',null,identifiers)
-    rownum++
-*/
     nl=csv.readNext()
   }
 }
 
 
-
-def queryZdbId(def httpbuilder, def config, def zdbId){
-	def url = "${config.path}/api/find?componentType=Journal&identifier=${zdbId}"
-	println "URI: ${url}"
-	httpbuilder.request(Method.GET) { req ->
-      uri.path=url
-      accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-      // requestContentType = ContentType.JSON
-
-      response.success = { resp, json ->
-        return json
-      }
-      response.failure = { resp ->
-        return []
-      }
-    }
+/**
+ * in accordance with FIZE-164
+ */
+String getZdbCheckDigit(def zdbId) {
+  if (!((String.valueOf(zdbId)).matches("[\\d]{7}"))){
+    return null
+  }
+  int number = Integer.valueOf(zdbId)
+  String x = 'X'
+  int checkDigit = 0
+  def factor = 2
+  while (number > 0){
+    checkDigit += (number % 10) * factor
+    number = number / 10
+    factor++
+  }
+  checkDigit %= 11
+  return String.valueOf (( checkDigit == 10) ? x : checkDigit)
 }
 
 
 
-def addToGoKB(dryrun, gokb, title, type, publisher, ids) {
+List getRecords(def config, def zdbId){
+  def url = "http://localhost:${config.port}"
+  def path = "${config.path}/api/find?componentType=Journal&identifier=${zdbId}"
 
-  /*
-   * TODO
-   *
-    def title_data = [e
-    type:type,
-    title:title,
-    publisher:publisher,
-    identifiers:ids,
-    status:'Expected'
-  ]
+  println ("Getting record by URL: ${url.concat(path)}")
 
-  if ( dryrun ) {
-    println("add title : ${title} ${type} ${publisher} ${ids}")
+  http = new HTTPBuilder(url.concat(path))
+  http.get(contentType : ContentType.TEXT) { resp, reader ->
+    def text = reader.getText()
+    def jsonSlurper = new JsonSlurper()
+    def map = jsonSlurper.parseText(text)
+    println ("... got: ${map}")
+    return map
   }
-  else {
-    gokb.request(Method.POST) { req ->
-      uri.path='${config.path}/integration/crossReferenceTitle'
-      body = title_data
-      requestContentType = ContentType.JSON
+  return []
+}
 
-      response.success = { resp ->
-        println "Success! ${resp.status}"
-      }
 
-      response.failure = { resp ->
-        println "Request failed with status ${resp.status}"
-      }
+
+def addToGoKB(httpbuilder, rec, config){
+
+  println("Requesting crossReferenceTitle for record: ${rec}")
+
+  httpbuilder.request(Method.POST) { req ->
+    uri.path='${config.path}/integration/crossReferenceTitle'
+    body = rec
+    requestContentType = ContentType.JSON
+
+    println("Adding record: ${rec}")
+
+    response.success = { resp ->
+      println "Added EZB ID for record: ${rec}"
     }
-  } */
 
+    response.failure = { resp ->
+      println "Couldn't add EZB ID for record: ${rec}"
+      println "... status was: ${resp.status}"
+    }
+  }
 }
