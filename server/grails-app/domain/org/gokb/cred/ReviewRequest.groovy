dd1ff114d3a3296ba38bdfcc6a19c38fb89a1ffb
Moritz Horn <moritz.horn@gbv.de>
2025-07-15 17:20:28 +0200

    add editing notes

--- server/grails-app/domain/org/gokb/cred/ReviewRequest.groovy
+++ server/grails-app/domain/org/gokb/cred/ReviewRequest.groovy
@@ -23,6 +23,7 @@ class ReviewRequest {
   Boolean needsNotify
   RefineProject refineProject
   String additionalInfo
+  String editingNotes
 
   // Timestamps
   Date dateCreated
@@ -33,6 +34,7 @@ class ReviewRequest {
     descriptionOfCause column:'rr_cause_txt', type:'text'
     reviewRequest column:'rr_req_txt', type:'text'
     additionalInfo column:'rr_additional_info', type:'text'
+    editingNotes column:'rr_editing_notes', type:'text'
   }
 
   static constraints = {
