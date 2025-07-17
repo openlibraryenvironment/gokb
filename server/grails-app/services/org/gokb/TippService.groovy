565125e9b91591fcd630f5875d586b98d65dd5e0
Moritz Horn <moritz.horn@gbv.de>
2025-07-17 11:35:38 +0200

    move tipl ensure to service

--- server/grails-app/services/org/gokb/TippService.groovy
+++ server/grails-app/services/org/gokb/TippService.groovy
@@ -997,7 +997,8 @@ class TippService {
         }
 
         if (ti) {
-          new Combo(fromComponent: ti, toComponent: tipp, type: RefdataCategory.lookup('Combo.Type', 'TitleInstance.Tipps')).save(flush: true)
+          tipp.title = ti
+          tipp.save(flush: true)
 
           if (result.status == 'matched') {
             boolean ti_changed = componentUpdateService.updateIdentifiers(ti, tipp_ids)
@@ -1013,7 +1014,7 @@ class TippService {
           tipp.lastSeen = System.currentTimeMillis()
           tipp.save(flush: true)
 
-          TitleInstancePlatform.ensure(ti, tipp.hostPlatform, tipp.url)
+          ensureTipl(ti, tipp.hostPlatform, tipp.url)
 
           touchPackage(tipp)
 
@@ -2100,5 +2101,45 @@ class TippService {
 
     duplicate.deleteSoft()
     touchPackage(target)
+  }
+
+  def ensureTipl(title, platform, url) {
+    if ( ( title != null ) && ( platform != null ) && ( url?.trim()?.length() > 0 ) ) {
+      def status_current = RefdataCategory.lookup('KBComponent.Status', 'Current')
+      def r = TitleInstancePlatform.executeQuery('''select tipl
+              from TitleInstancePlatform as tipl,
+              Combo as titleCombo,
+              Combo as platformCombo
+              where titleCombo.toComponent = tipl
+              and titleCombo.fromComponent = :ti
+              and platformCombo.toComponent = tipl
+              and platformCombo.fromComponent = :plt
+              and tipl.status = :sc
+              ''',[ti: title, plt: platform, sc: status_current])
+
+      if ( r.size() == 0 ) {
+        def tipl = new TitleInstancePlatform(url:url).save(flush:true, failOnError:true)
+
+        def plt_combo_type = RefdataCategory.lookup('Combo.Type', 'Platform.HostedTitles')
+        def plt_combo = new Combo(toComponent:tipl, fromComponent:platform, type:plt_combo_type).save(flush:true, failOnError:true);
+
+        def ti_combo_type = RefdataCategory.lookup('Combo.Type', 'TitleInstance.Tipls')
+        def ti_combo = new Combo(toComponent:tipl, fromComponent:title, type:ti_combo_type).save(flush:true, failOnError:true);
+
+        return tipl
+
+      } else if ( r.size() == 1 ) {
+        def matched_tipl = r[0]
+
+        if (url && matched_tipl.url != url) {
+          matched_tipl.url = url
+        }
+        return matched_tipl
+
+      } else {
+        log.warn("Found more than one TIPL for ${title} on ${platform}!")
+        return null
+      }
+    }
   }
 }
