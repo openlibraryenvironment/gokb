9edb89ba72945a4eff1918216312fb80484d11bd
Moritz Horn <moritz.horn@gbv.de>
2025-09-10 15:11:04 +0200

    handle uuid links in tipp upsert

--- server/grails-app/services/org/gokb/TippUpsertService.groovy
+++ server/grails-app/services/org/gokb/TippUpsertService.groovy
@@ -69,6 +69,9 @@ class TippUpsertService {
       if (pkg_info instanceof Map) {
         pkg = Package.get(pkg_info.id ?: pkg_info.internalId)
       }
+      else if (pkg_info instanceof String) {
+        pkg = Package.findByUuid(pkg_info)
+      }
       else {
         pkg = Package.get(pkg_info)
       }
@@ -82,6 +85,9 @@ class TippUpsertService {
       if (plt_info instanceof Map) {
         plt = Platform.get(plt_info.id ?: plt_info.internalId)
       }
+      else if (plt_info instanceof String) {
+        plt = Platform.findByUuid(plt_info)
+      }
       else {
         plt = Platform.get(plt_info)
       }
@@ -94,6 +100,9 @@ class TippUpsertService {
 
       if (title_info instanceof Map) {
         ti = TitleInstance.get(title_info.id ?: title_info.internalId)
+      }
+      else if (title_info instanceof String) {
+        ti = TitleInstance.findByUuid(title_info)
       }
       else {
         ti = TitleInstance.get(title_info)
