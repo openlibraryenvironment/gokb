643d11c9693adb33f79d2359a4f6b057196cb028
christophkohl1 <christoph.kohl@hbz-nrw.de>
2025-12-03 17:56:48 +0100

    remove pw encryption

--- server/grails-app/conf/spring/resources.groovy
+++ server/grails-app/conf/spring/resources.groovy
@@ -1,14 +1,11 @@
 import com.k_int.UserPasswordEncoderListener
-import com.k_int.WebEndpointPasswordEncoderListener
+
 import com.k_int.utils.DatabaseMessageSource;
 import org.grails.spring.context.support.PluginAwareResourceBundleMessageSource;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
 
 beans = {
   userPasswordEncoderListener(UserPasswordEncoderListener)
-
-  // Passwords must be provided in plain Text to FTP Server
-  //webEndpointPasswordEncoderListener(WebEndpointPasswordEncoderListener)
 
   messageBundleMessageSource(PluginAwareResourceBundleMessageSource) {
     basenames = ["WEB-INF/grails-app/i18n/messages"]
