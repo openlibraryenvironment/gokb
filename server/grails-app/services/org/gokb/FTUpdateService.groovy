a08d6e15a459f841e5660cbb0690cb499d1a55a3
Moritz Horn <moritz.horn@gbv.de>
2025-12-08 18:32:59 +0100

    adjust bulk index pagination

--- server/grails-app/services/org/gokb/FTUpdateService.groovy
+++ server/grails-app/services/org/gokb/FTUpdateService.groovy
@@ -566,12 +566,12 @@ class FTUpdateService {
 
         def total = 0
         Date from = new Date(latest_ft_record.lastTimestamp)
-        def countq = domain.executeQuery("select count(o.id) from " + domain.name + " as o where (( o.lastUpdated > :ts ) OR ( o.dateCreated > :ts )) ", [ts: from], [readonly: true])[0]
+        def countq = domain.executeQuery("select count(o.id) from " + domain.name + " as o where (o.lastUpdated > :ts OR (o.lastUpdated = :ts AND o.id > :lid) OR o.dateCreated > :ts) ", [ts: from, lid: latest_ft_record.lastId], [readonly: true])[0]
 
         if (job) job.message("Indexing start for ${countq} ${domain.simpleName} ..".toString())
 
         log.debug("Will process ${countq} records")
-        def q = domain.executeQuery("select o.id from " + domain.name + " as o where ((o.lastUpdated > :ts ) OR ( o.dateCreated > :ts )) order by o.lastUpdated, o.id", [ts: from], [readonly: true])
+        def q = domain.executeQuery("select o.id from " + domain.name + " as o where (o.lastUpdated > :ts OR (o.lastUpdated = :ts AND o.id > :lid) OR o.dateCreated > :ts) order by o.lastUpdated, o.id", [ts: from, lid: latest_ft_record.lastId], [readonly: true])
         log.debug("Query completed.. processing rows...")
         BulkRequest bulkRequest = new BulkRequest()
 
