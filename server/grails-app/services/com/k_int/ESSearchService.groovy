97b25dc951de7379aebdf91b034c9dbe5aca0934
Moritz Horn <moritz.horn@gbv.de>
2025-12-10 18:53:39 +0100

    custom url handling

--- server/grails-app/services/com/k_int/ESSearchService.groovy
+++ server/grails-app/services/com/k_int/ESSearchService.groovy
@@ -662,27 +662,55 @@ class ESSearchService{
       if (qpars.qfields){
         List allQFields = (requestMapping.generic + requestMapping.refdata + requestMapping.simpleMap.values() +
                            requestMapping.complex)
-        for (String field in qpars.list('qfields')){
-          if (field == "name") {
-            genericQuery.should(QueryBuilders.matchQuery("name", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(qpars.primaryUrl ? 1000f : 8f))
-            genericQuery.should(QueryBuilders.matchQuery("normname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(2f))
-          }
-          else if (field == "altname") {
-            genericQuery.should(QueryBuilders.matchQuery("altname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND))
-          }
-          else if (field == "suggest") {
-            genericQuery.should(QueryBuilders.matchQuery("suggest", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(0.6f))
-            genericQuery.should(QueryBuilders.matchQuery("normSuggest", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(0.6f))
-          }
-          else if (field in allQFields){
-            if (field.contains('Url')) {
-              String final_url_qry = sanitizeParam(qpars.q.startsWith('https') ? qpars.q.replace(/https?(:\/\/)?/, '') : qpars.q)
 
-              genericQuery.should(QueryBuilders.matchQuery(field, final_url_qry).boost(0.5f).fuzzyTranspositions(false).autoGenerateSynonymsPhraseQuery(false))
-              genericQuery.should(QueryBuilders.matchPhraseQuery(field, sanitized_param).boost(4f))
+        if (qpars.list('qfields').contains('primaryUrl')) {
+          String final_url_qry = sanitizeParam(qpars.q.startsWith('http') ? qpars.q.replace(/https?(:\/\/)?/, '') : qpars.q)
+
+          if (qpars.q.startsWith('http')) {
+
+            genericQuery.should(QueryBuilders.matchQuery("name", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND))
+            genericQuery.should(QueryBuilders.matchQuery("normname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(0.5f))
+            genericQuery.should(QueryBuilders.matchQuery("altname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND)boost(0.5f))
+
+            genericQuery.should(QueryBuilders.matchQuery('primaryUrl', final_url_qry).boost(0.5f).fuzzyTranspositions(false))
+
+            genericQuery.should(QueryBuilders.matchPhraseQuery('primaryUrl', sanitized_param).boost(10))
+          }
+          else {
+            genericQuery.should(QueryBuilders.matchQuery("name", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(3f))
+            genericQuery.should(QueryBuilders.matchQuery("normname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(2f))
+            genericQuery.should(QueryBuilders.matchQuery("altname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND))
+            genericQuery.should(QueryBuilders.matchQuery("suggest", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(0.2f))
+            genericQuery.should(QueryBuilders.matchQuery("normSuggest", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(0.2f))
+            genericQuery.should(QueryBuilders.matchQuery('primaryUrl', final_url_qry).boost(0.05f).fuzzyTranspositions(false))
+          }
+        }
+        else {
+          for (String field in qpars.list('qfields')) {
+            if (field == "name") {
+              genericQuery.should(QueryBuilders.matchQuery("name", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(8f))
+              genericQuery.should(QueryBuilders.matchQuery("normname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(2f))
             }
-            else {
-              genericQuery.should(QueryBuilders.matchQuery(field, sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND))
+            else if (field == "altname") {
+              genericQuery.should(QueryBuilders.matchQuery("altname", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND))
+            }
+            else if (field == "suggest") {
+              genericQuery.should(QueryBuilders.matchQuery("suggest", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(0.2f))
+              genericQuery.should(QueryBuilders.matchQuery("normSuggest", sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND).boost(0.2f))
+            }
+            else if (field in allQFields){
+              if (field.contains('Url')) {
+                String final_url_qry = sanitizeParam(qpars.q.startsWith('http') ? qpars.q.replace(/https?(:\/\/)?/, '') : qpars.q)
+
+                if (qpars.q.startsWith('http')) {
+                  genericQuery.should(QueryBuilders.matchQuery(field, final_url_qry).boost(0.5f).fuzzyTranspositions(false))
+                }
+
+                genericQuery.should(QueryBuilders.termQuery(field, sanitized_param).boost(8))
+              }
+              else {
+                genericQuery.should(QueryBuilders.matchQuery(field, sanitized_param).operator(defaultOr ? Operator.OR : Operator.AND))
+              }
             }
           }
         }
